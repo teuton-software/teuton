f946e3718d0216d2d506bf058a5273936040cf6e
David Vargas <davidvargas.tenerife@gmail.com>
2015-10-06 16:05:55 +0100

    checks for add1516

---
+++ check/add1516/clientes-ligeros.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require_relative '../../lib/tool'
+
+=begin
+ Módulo ADD1516. Actividad Clientes ligeros
+=end
+
+define_test :hostnames do
+
+	description "Checking SSH service on <"+get(:host1_ip)+">"
+	command "nmap #{get(:host1_ip)} | grep 22|wc -l"
+	run_on :localhost
+	check result.to_i.equal?(1)
+
+	description "Checking hostname <"+get(:host1_hostname)+">"
+	command "hostname -f"
+	run_on :host1
+	check result.equal?(get(:host1_hostname))
+end
+
+define_test :users do
+	
+	description "Users <#{get(:apellido1)}[123]>"
+	command "cat /etc/passwd | grep #{get(:apellido1)}| wc -l"
+	run_on :host1
+	check result.to_i.equal?(3)
+	
+	description "Users passwd <#{get(:apellido1)}[123]>"
+	command "cat /etc/shadow | grep #{get(:apellido1)}| cut -d : -f 2| wc -l"
+	run_on :host1
+	check result.to_i.equal?(3)
+
+end
+
+define_test :cliente_ligero do
+	description "Cliente ligero 192.168.0.20"
+	command "arp | grep 192.168.0.20|grep eth1"
+	run_on :host1
+	check result.to_i.equal?(1)
+	
+	description "Cliente ligero 192.168.0.21"
+	command "arp | grep 192.168.0.21|grep eth1"
+	run_on :host1
+	check result.to_i.equal?(1)
+
+end
+
+start do
+	show :resume
+	export :all
+end
