17bb52e15fe8268d8c357adcb0eadbdfa18474a6
David Vargas <davidvargas.tenerife@gmail.com>
2016-02-09 22:04:39 +0000

    U replace method on by goto

--- check/add/ssh-conections.rb
+++ check/add/ssh-conections.rb
@@ -10,146 +10,146 @@ require_relative '../../lib/tool'
 =end
 
 check :configurations do
-  on :host1, :execute => "ifconfig| grep HWaddr| tr -s ' ' '%'| cut -d % -f 5"
+  goto :host1, :execute => "ifconfig| grep HWaddr| tr -s ' ' '%'| cut -d % -f 5"
   unique "MAC[host1]", result.value 
 
-  on :host2, :execute => "ifconfig| grep HWaddr| tr -s ' ' '%'| cut -d % -f 5"
+  goto :host2, :execute => "ifconfig| grep HWaddr| tr -s ' ' '%'| cut -d % -f 5"
   unique "MAC[host2]", result.value
 end
 
 check :debian_configurations do	
   desc "Zona horaria <WET #{get(:year)}>"
-  on :host1, :execute => "date | grep WET | grep #{get(:year)} | wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "date | grep WET | grep #{get(:year)} | wc -l"
+  expect result.equal?(1)
 
   desc "OS Debian 64 bits"
-  on :host1, :execute => "uname -a| grep Debian| grep 64| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "uname -a| grep Debian| grep 64| wc -l"
+  expect result.equal?(1)
 
   desc "Cheking hostname <"+get(:host1_hostname)+">"
-  on :host1, :execute => "hostname -a"
-  expect result.to_s.equal?(get(:host1_hostname))
+  goto :host1, :execute => "hostname -a"
+  expect result.equal?(get(:host1_hostname))
 	
   desc "Checking domainname <"+get(:lastname)+">"
-  on :host1, :execute =>  "hostname -d" 
-  expect result.to_s.equal?(get(:lastname))
+  goto :host1, :execute =>  "hostname -d" 
+  expect result.equal?(get(:lastname))
 	
   desc "Checking username <"+get(:firstname)+">"
-  on :host1, :execute => "cat /etc/passwd|grep '"+get(:firstname)+"'|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "cat /etc/passwd|grep '"+get(:firstname)+"'|wc -l"
+  expect result.equal?(1)
 
   desc "Checking groupname <"+get(:groupname)+">"
-  on :host1,  :execute => "cat /etc/group|grep '"+get(:groupname)+"'|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1,  :execute => "cat /etc/group|grep '"+get(:groupname)+"'|wc -l"
+  expect result.equal?(1)
 
   desc "User member of group <"+get(:groupname)+">"
-  on :host1, :execute => "id "+get(:firstname)+"| tr -s ' ' ':'| cut -d : -f 2| grep "+get(:groupname)+"|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "id "+get(:firstname)+"| tr -s ' ' ':'| cut -d : -f 2| grep "+get(:groupname)+"|wc -l"
+  expect result.equal?(1)
 end
 
 check :ssh_configurations do
   desc "Claves privada y pública en usuario <#{get(:firstname)}>"
-  on :host1, :execute => "vdir /home/#{get(:firstname)}/.ssh/id_*| wc -l"
-  expect result.to_i.equal?(2)
+  goto :host1, :execute => "vdir /home/#{get(:firstname)}/.ssh/id_*| wc -l"
+  expect result.equal?(2)
 
-  on :host1, :execute => "cat /home/#{get(:firstname)}/.ssh/id_rsa.pub", :tempfile => "mv1_idrsapub.tmp"
+  goto :host1, :execute => "cat /home/#{get(:firstname)}/.ssh/id_rsa.pub", :tempfile => "mv1_idrsapub.tmp"
   @filename1=tempfile
   	
   desc "Host2 hostname defined on Host1"
-  on :host1, :execute => "cat /etc/hosts| grep #{get(:host2_hostname)}| grep #{get(:host2_ip)}| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "cat /etc/hosts| grep #{get(:host2_hostname)}| grep #{get(:host2_ip)}| wc -l"
+  expect result.equal?(1)
 end
 
 check :opensuse_configurations do
   desc "Zona horaria <WET #{get(:year)}>"
-  on :host2, :execute => "date | grep WET | grep #{get(:year)} | wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "date | grep WET | grep #{get(:year)} | wc -l"
+  expect result.equal?(1)
 
   desc "OS OpenSuse 64 bits"
-  on :host2, :execute => "uname -a| grep opensuse| grep x86_64| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "uname -a| grep opensuse| grep x86_64| wc -l"
+  expect result.equal?(1)
 
   desc "Value hostname == "+get(:host2_hostname)+" "
-  on :host2, :execute => "hostname -a"
-  expect result.to_s.equal?(get(:host2_hostname))
+  goto :host2, :execute => "hostname -a"
+  expect result.equal?(get(:host2_hostname))
 	
   desc "Value domainname == "+get(:lastname)+" "
-  on :host2, :execute => "hostname -d" 
-  expect result.to_s.equal?(get(:lastname))
+  goto :host2, :execute => "hostname -d" 
+  expect result.equal?(get(:lastname))
 
   desc "Exist username <"+get(:firstname)+">"
-  on :host2, :execute => "cat /etc/passwd|grep '"+get(:firstname)+"'|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "cat /etc/passwd|grep '"+get(:firstname)+"'|wc -l"
+  expect result.equal?(1)
 
   desc "Checking groupname <"+get(:groupname)+">"
-  on :host1, :execute => "cat /etc/group|grep '"+get(:groupname)+"'|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "cat /etc/group|grep '"+get(:groupname)+"'|wc -l"
+  expect result.equal?(1)
 
   desc "User maingroup == "+get(:groupname)+" "
-  on :host1, :execute => "id "+get(:firstname)+"| tr -s ' ' ':'| cut -d : -f 2| grep "+get(:groupname)+"|wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "id "+get(:firstname)+"| tr -s ' ' ':'| cut -d : -f 2| grep "+get(:groupname)+"|wc -l"
+  expect result.equal?(1)
 end
 
 check :ssh_configurations_on_host2 do
   desc "Permissions /home/#{get(:firstname)}/.ssh => rwx------"
-  on :host2, :expect => "vdir -a /home/#{get(:firstname)}/ | grep '.ssh'| grep 'rwx------'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "vdir -a /home/#{get(:firstname)}/ | grep '.ssh'| grep 'rwx------'| wc -l"
+  expect result.equal?(1)
 
-  on :host2, :execute => "cat /home/#{get(:firstname)}/.ssh/authorized_keys", :tempfile => "mv2_authorizedkeys.tmp"
+  goto :host2, :execute => "cat /home/#{get(:firstname)}/.ssh/authorized_keys", :tempfile => "mv2_authorizedkeys.tmp"
   filename2=tempfile
   	
   desc "mv2(authorized_keys) == mv1(id_rsa.pub)"
-  on :localhost, :execute => "diff #{@filename1} #{filename2}| wc -l"
-  expect result.to_i.equal?(0)
+  goto :localhost, :execute => "diff #{@filename1} #{filename2}| wc -l"
+  expect result.equal?(0)
 
   desc "mv2: Host1 hostname defined"
-  on :host2, :execute => "cat /etc/hosts| grep #{get(:host1_hostname)}| grep #{get(:host1_ip)}| wc -l"
-  expect result.to_i.equal?(1)	
+  goto :host2, :execute => "cat /etc/hosts| grep #{get(:host1_hostname)}| grep #{get(:host1_ip)}| wc -l"
+  expect result.equal?(1)	
 end
 
 check :vncserver_configurations_on_host1 do
   desc "Tightvncserver installed on <#{get(:host1_ip)}>"
-  on :host1, :execute => "dpkg -l tightvncserver| grep 'ii'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "dpkg -l tightvncserver| grep 'ii'| wc -l"
+  expect result.equal?(1)
 
-  on :localhost, :execute => "nmap -Pn #{get(:host1_ip)}", :tempfile => 'mv1_nmap.tmp'
+  goto :localhost, :execute => "nmap -Pn #{get(:host1_ip)}", :tempfile => 'mv1_nmap.tmp'
   filename = tempfile
   
   #command "ps -ef| grep tightvnc| grep geometry| wc -l", :tempfile => 'tightvnc.tmp'
   #vncserver :1
 	
   desc "Services 'vnc' on <#{get(:host1_ip)}>"
-  on :localhost, :execute => "cat #{filename}|grep 'vnc'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :localhost, :execute => "cat #{filename}|grep 'vnc'| wc -l"
+  expect result.equal?(1)
 
   desc "Services active on ip/port #{get(:host1_ip)}/6001"
-  on :localhost, :execute => "cat #{filename} |grep '6001'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :localhost, :execute => "cat #{filename} |grep '6001'| wc -l"
+  expect result.equal?(1)
 end
 
 check :git_clone_on_host2 do
   desc "Git installed on <#{get(:host1_ip)}>"
-  on :host1, :execute => "dpkg -l git| grep 'ii'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "dpkg -l git| grep 'ii'| wc -l"
+  expect result.equal?(1)
 	
   #desc "Proyect from GitHub cloned on <#{get(:host1_ip)}>"
   #on :host1, :execute => "vdir -a /home/#{get(:username)}/add1314profesor/ | grep '.git'| wc -l"
   #check result.to_i.equal?(1)
 
   desc "Git installed on <#{get(:host2_ip)}>"
-  on :host2, :execute => "git --version| grep git |grep version| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "git --version| grep git |grep version| wc -l"
+  expect result.equal?(1)
 	
   desc "Proyect from GitHub cloned on <#{get(:host2_ip)}>"
-  on :host2, :execute => "vdir -a /home/#{get(:firstname)}/add1314profesor/unit.1/ | grep '.exam_remoto'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host2, :execute => "vdir -a /home/#{get(:firstname)}/add1314profesor/unit.1/ | grep '.exam_remoto'| wc -l"
+  expect result.equal?(1)
 
   log "Tests finished"
 end
 
 start do
-	show :resume
-	export :all, :format => :txt
+	show
+	export
 end
 
 =begin
--- check/add/thin-clients.rb
+++ check/add/thin-clients.rb
@@ -10,15 +10,15 @@ require_relative '../../lib/tool'
 
 check :hostnames_configurations do
   desc "Checking SSH port <"+get(:host1_ip)+">"
-  on :localhost, :execute => "nmap #{get(:host1_ip)} | grep ssh|wc -l"
-  expect result.to_i.equal?(1)
+  goto :localhost, :execute => "nmap #{get(:host1_ip)} | grep ssh|wc -l"
+  expect result.equal?(1)
 
   desc "Checking hostname <"+get(:host1_hostname)+">"
-  on :host1, :execute => "hostname -f"
+  goto :host1, :execute => "hostname -f"
   expect result.equal?(get(:host1_hostname))
 
   unique "hostname", result.value	
-  on :host1, :execute => "blkid |grep sda1"
+  goto :host1, :execute => "blkid |grep sda1"
   unique "UUID", result.value	
 end
 
@@ -29,29 +29,29 @@ check :users_definitions do
     username=get(:apellido1)+i.to_s
 
 	desc "User <#{username}> exists"
-	on :host1, :execute => "cat /etc/passwd | grep #{username} | wc -l"
-	expect result.to_i.equal?(1)
+	goto :host1, :execute => "cat /etc/passwd | grep #{username} | wc -l"
+	expect result.equal?(1)
 
 	desc "Users <#{username}> with not empty password "
-	on :host1, :execute => "cat /etc/shadow | grep #{username}| cut -d : -f 2| wc -l"
-	expect result.to_i.equal?(1)
+	goto :host1, :execute => "cat /etc/shadow | grep #{username}| cut -d : -f 2| wc -l"
+	expect result.equal?(1)
 
 	desc "User <#{username}> logged"
-	on :host1, :execute => "last | grep #{username[0,8]} | wc -l"
-	expect result.to_i.not_equal?(0)
+	goto :host1, :execute => "last | grep #{username[0,8]} | wc -l"
+	expect result.not_equal?(0)
   end
 end
 
 check :software_installed do
   desc "Package <#{packagename}> installed"
-  on :host1, :execute => "dpkg -l #{packagename}| grep ii| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "dpkg -l #{packagename}| grep ii| wc -l"
+  expect result.equal?(1)
 
   desc "Image builded"
-  on :host1, :execute => "ltsp-info| grep 'found image'| wc -l"
-  expect result.to_i.equal?(1)
+  goto :host1, :execute => "ltsp-info| grep 'found image'| wc -l"
+  expect result.equal?(1)
 
-  on :host1, :execute => "ltsp-info| grep 'found image'| tr -s ':' ' '|tr -s ' ' ':'| cut -d : -f 3"
+  goto :host1, :execute => "ltsp-info| grep 'found image'| tr -s ':' ' '|tr -s ' ' ':'| cut -d : -f 3"
   log imagefullname=result.value
 end
 
@@ -60,18 +60,18 @@ check :services_are_running do
   
   services.each do |service|
     desc "Service <#{service}> running"
-    on :host1, :execute => "ps -ef| grep #{service}| grep -v color| wc -l"
-    expect result.to_i.equal?(1)
+    goto :host1, :execute => "ps -ef| grep #{service}| grep -v color| wc -l"
+    expect result.equal?(1)
   end
   
   filename='/etc/default/isc-dhcp-server'
   desc "<#{filename}> content"
-  on :host1, :execute => "cat #{filename}|grep INTERFACES"
+  goto :host1, :execute => "cat #{filename}|grep INTERFACES"
   expect result.value.include? 'INTERFACES="eth1"'
 
   filename='/etc/default/tftpd-pha'
   desc "<#{filename}> content"
-  on :host1, :execute => "cat #{filename}|grep TFTP_ADDRESS"
+  goto :host1, :execute => "cat #{filename}|grep TFTP_ADDRESS"
   expect result.value.include? 'TFTP_ADDRESS="192.168.0.1:69"'
 end
 
@@ -82,23 +82,36 @@ check :thin_clients_are_running do
     ip="192.168.0."+i.to_s
 	
 	desc "Thin client #{ip} into ARP table"
-	on :host1, :execute => "arp | grep #{ip}| wc -l"
-	expect result.to_i.equal?(1)
+	goto :host1, :execute => "arp | grep #{ip}| wc -l"
+	expect result.equal?(1)
 
 	desc "Thin client #{ip} into LOG files"
-	on :host1, :execute => "cat /var/log/syslog |grep dhcp |grep DHCPREQUEST |grep #{ip} |wc -l"
-	expect result.to_i.is_greater_than? 1 
+	goto :host1, :execute => "cat /var/log/syslog |grep dhcp |grep DHCPREQUEST |grep #{ip} |wc -l"
+	expect result.is_greater_than? 1 
 
 	desc "Thin client #{ip} into LOG files"
-	on :host1, :execute => "cat /var/log/syslog |grep dhcp |grep DHCPACK |grep #{ip} |wc -l"
-	expect result.to_i.is_greater_than? 1 
+	goto :host1, :execute => "cat /var/log/syslog |grep dhcp |grep DHCPACK |grep #{ip} |wc -l"
+	expect result.is_greater_than? 1 
   end
   	
-  on :host1, :execute => "ip link | grep ether"
+  goto :host1, :execute => "ip link | grep ether"
   unique "LTSP MAC", result.value
 end
 
 start do
-	show :resume
-	export :all
+	show
+	export
 end
+
+=begin
+---
+:global:
+  :host1_username: root
+:cases:
+- :tt_members: aaa
+  :tt_emails: aaa@gmail.com
+  :host1_ip: 172.18.2.41
+  :host1_password: aaa
+  :host1_hostname: aaa.bbb
+  :apellido1: aaa
+=end
