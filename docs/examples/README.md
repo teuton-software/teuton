5f0a6d1f41fbfa9c1ba5fd6e01115499daf9802f
David Vargas <davidvargas.tenerife@gmail.com>
2016-02-11 11:03:48 +0000

    U example-03

--- docs/examples/README.md
+++ docs/examples/README.md
@@ -6,3 +6,4 @@
 |-------- | ----------- |------------ |
 |[example-01.rb](./example-01.rb) | [example-01.yaml](./example-01.yaml) | Test if exist user <obiwan> into localhost |
 |[example-02.rb](./example-02.rb) | [example-02.yaml](./example-02.yaml) | Test if exist username for every case into localhost |
+|[example-03.rb](./example-03.rb) | [example-03.yaml](./example-03.yaml) | Test several targets for every case into diferents hosts |
---
+++ docs/examples/example-03.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require_relative '../../lib/tool'
+
+=begin
+  Test several targets for every case into diferent hosts:
+  * desc: Describe the target
+  * goto: Move to localhost and execute the command
+  * expect: Check if the result is equal to the expected value
+  * get: Get the value for every case from the configuration YAML file.
+=end
+
+check :configurations do
+
+  desc "Exist user <"+get(:username)+">"
+  goto :host1, :execute => "id #{get(:username)} |wc -l"
+  expect result.equal?(1)
+
+  desc "Hostname is <"+get(:host1_hostname)+">"
+  goto :host1, :execute => "hostname -f"
+  expect result.equal?(get(:host1_hostname))
+
+  desc "DNS Server OK"
+  goto :host1, :execute => "host www.google.es| grep 'has address'"
+  expect result.equal?(1)
+
+end
+
+start do
+	show
+	export
+end
