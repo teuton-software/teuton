30852b2e13148ce7e9262e491088685a71f92fb6
David Vargas <davidvargas.tenerife@gmail.com>
2016-04-05 13:06:01 +0100

    N split files

--- check/idp1516/nagios-debian.rb
+++ check/idp1516/nagios-debian.rb
@@ -13,6 +13,7 @@
 
 require_relative 'nagios-debian/debian1-config-mv'
 require_relative 'nagios-debian/debian1-nagios-server'
+require_relative 'nagios-debian/debian2-config-mv'
 require_relative 'nagios-debian/debian2-agent'
 #require_relative 'nagios-debian/windows-agent'
 
