64658e8bf6a3ee53c30d21988e4fa49cd8a0b86a
David Vargas <davidvargas.tenerife@gmail.com>
2016-04-07 17:33:33 +0100

    F example windows ok

--- docs/examples/example-windows.rb
+++ docs/examples/example-windows.rb
@@ -3,11 +3,11 @@
 task "Windows: external configuration" do
 
   target "Localhost: Conection with <#{get(:windows1_ip)}>"
-  goto :localhost, :exec => "ping #{get(:windows1_ip)} -c 1| grep 'Destination Host Unreachable'|wc -l"
+  goto   :localhost, :exec => "ping #{get(:windows1_ip)} -c 1| grep 'Destination Host Unreachable'|wc -l"
   expect result.eq 0
   
   target "Localhost: netbios-ssn service on #{get(:windows1_ip)}"
-  goto :localhost, :exec => "nmap -Pn #{get(:windows1_ip)} | grep '139/tcp'| grep 'open'|wc -l"
+  goto   :localhost, :exec => "nmap -Pn #{get(:windows1_ip)} | grep '139/tcp'| grep 'open'|wc -l"
   expect result.eq 1
 
 end
@@ -15,29 +15,24 @@ end
 task "Windows: internal configurations" do
 
   target "Windows version"
-  goto :windows1, :exec => "ver"
+  goto   :windows1, :exec => "ver"
   expect result.find!("Windows").find!("6.1").count!.eq 1
 
   target "Windows COMPUTERNAME"
-  goto :windows1, :exec => "set"
-  result.debug
+  goto   :windows1, :exec => "set"
   expect result.find!("COMPUTERNAME").find!(get(:windows1_hostname)).count!.eq 1
 
   target "Windows router Config<#{get(:gateway)}>"
-  goto :windows1, :exec => "ipconfig"
+  goto   :windows1, :exec => "ipconfig"
   expect result.find!("enlace").find!(get(:gateway)).count!.eq 1
 
-#  target "Windows router OK"
-#  goto :windows1, :exec => "ping 8.8.4.4"
-#  result.debug
-##  result.find!(get(:dns))
-#  result.debug
-#  expect result.count!.eq 4
+  target "Windows router OK"
+  goto   :windows1, :exec => "ping #{get(:dns)}"
+  expect result.find!("Respuesta").count!.eq 4
 
-#  target "Windows DNS OK"
-#  goto :windows1, :exec => "nslookup www.iespuertodelacruz.es"
-#  expect result.find!("Address:").find!("88.198.18.148").count!.eq 1
-
+  target "Windows DNS OK"
+  goto   :windows1, :exec => "nslookup www.iespuertodelacruz.es"
+  expect result.find!("Address:").find!("88.198.18.148").count!.eq 1
 end
 
 =begin
