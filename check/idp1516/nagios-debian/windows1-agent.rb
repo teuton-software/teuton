7ce93ff834461ad7a31e2d3468e85a8e16e45705
David Vargas <davidvargas.tenerife@gmail.com>
2016-04-07 12:09:39 +0100

    U windows targets

--- check/idp1516/nagios-debian/windows1-agent.rb
+++ check/idp1516/nagios-debian/windows1-agent.rb
@@ -15,37 +15,30 @@ task "Windows1 external configuration" do
 
 end
 
-
 task "Windows1 internal configurations" do
 
   target "Windows1 version"
   goto :windows1, :exec => "ver"
   expect result.find!("Windows").find!("6.1").count!.eq 1
-end
 
-=begin
   target "Windows1 COMPUTERNAME"
   goto :windows1, :exec => "set"
-  expect result.grep!("COMPUTERNAME").grep!(@short_hostname[3]).size!.eq 1
+  expect result.find!("COMPUTERNAME").find!(@short_hostname[3]).count!.eq 1
 
-  #ipconfig => IPv4, subred
-
-  target "Windows1 enlace"
+  target "Windows1 enlace <#{get(:bender_ip)}>"
   goto :windows1, :exec => "ipconfig"
-  expect result.grep!("enlace").grep!(get(:bender_ip)).size!.eq 1
+  expect result.find!("enlace").find!(get(:bender_ip)).count!.eq 1
 
   target "Windows1 router OK"
   goto :windows1, :exec => "ping 8.8.4.4"
-  expect result.grep!("Respuesta").size!.gt 1
+  expect result.find!("Respuesta").count!.gt 1
 
   target "Windows1 DNS OK"
   goto :windows1, :exec => "nslookup www.iespuertodelacruz.es"
-  expect result.grep!("Address:").grep!("88.198.18.148").size!.eq 1
+  expect result.find!("Address:").find!("88.198.18.148").count!.eq 1
 
 end
 
-#find cadena fichero
-#type muestra fichero
 =begin
 task "Ping from windows1 to *" do  
   target "ping windows1 to debian1_ip"
@@ -76,38 +69,46 @@ task "Windows: Configure Nagios Agent" do
   expect result.eq 1
 
   texts=[]
-  texts << "NRPEServer=1"
-  texts << "CheckSystem=1"
-  texts << "CheckDisk=1"
-  texts << "CheckExternalScripts=1"
+  texts << ["ssl options"  , "no-sslv2, no-sslv3"]
+  texts << ["verify mode"  , "none"]
+  texts << ["insecure"     , "true"]
+
+  texts << ["NRPEServer"          , "1"]
+  texts << ["CheckSystem"         , "1"]
+  texts << ["CheckDisk"           , "1"]
+  texts << ["CheckExternalScripts", "1"]
   
-  texts << "check_load=CheckCpu"
-  texts << "check_disk=CheckDriveSize"
-  texts << "check_firewall_service=CheckServiceState MpsSvc"
-  texts << "allowed hosts=#{get(:debian1_ip)}"
+  texts << ["check_load"             , "CheckCpu" ]
+  texts << ["check_disk"             , "CheckDriveSize" ]
+  texts << ["check_firewall_service" , "CheckServiceState MpsSvc" ]
+  texts << ["allowed hosts"          , get(:debian1_ip) ]
    
   texts.each do |text|
-    target "<#{file}> content: \"#{text}\""
-    goto :windows1, :exec => "cat #{file}| grep '#{text}' |wc -l"
-    expect result.eq 1
+    target "<#{file}> content: <#{text.join(",")}>"
+    goto   :windows1, :exec => "type #{file}"
+    expect result.find!(text[0]).find!(text[1]).count!.eq 1
   end
 end
 
-task "Restart Agent service on Debian2" do
+task "Windows1: Restart Agent service" do
 
-  target "Debian2: Stop agent service"
-  goto   :debian2, :exec => "service nagios-nrpe-server stop"
+  target "Windows1: Stop agent service"
+  goto   :debian2, :exec => "net stop nsclient"
   goto   :debian2, :exec => "service nagios-nrpe-server status |grep Active|grep inactive"
   expect result.eq 1
 
   target "Debian2: Start agent service"
-  goto   :debian2, :exec => "service nagios-nrpe-server start"
+  goto   :debian2, :exec => "net start nsclient"
   goto   :debian2, :exec => "service nagios-nrpe-server status |grep Active|grep active"
   expect result.eq(1), :weight => 2
   
+  target "Debian1 nmap to debian2"
+  goto :debian1, :exec => "nmap -Pn #{get(:debian2_ip)}"
+  expect result.find!("5666").find!("open").count!.eq(1), :weight => 5
+
   target "NRPE debian1 to windows1"
-  goto :debian1, :exec => "/usr/lib/nagios/plugins/check_nrpe -H #{get(:windows1_ip)} |wc -l"
-  expect result.eq 1
+  goto :debian1, :exec => "/usr/lib/nagios/plugins/check_nrpe -H #{get(:windows1_ip)}"
+  expect result.find!("fine").count!.eq 1
 
 end
 =end
