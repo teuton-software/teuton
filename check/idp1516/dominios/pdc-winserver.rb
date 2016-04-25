1e95f6866eb66ce892b1f061cbed9285ccc8db69
David Vargas <davidvargas.tenerife@gmail.com>
2016-04-25 13:21:25 +0100

    N winclients

--- check/idp1516/dominios/pdc-winserver.rb
+++ check/idp1516/dominios/pdc-winserver.rb
@@ -1,7 +1,12 @@
 
+=begin
+Spanish URL: https://github.com/dvarrui/libro-de-actividades/blob/master/actividades/idp/dominios/pdc-winserver.md
+=end
+
 require_relative 'winserver-config' 
+require_relative 'winclients-config' 
 
 start do
   show
-  report
+  export :format => :colored_text
 end
