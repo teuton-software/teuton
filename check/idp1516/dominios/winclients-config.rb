c6ad647db20be5d6db21ce67bb7fcc15369dffbb
David Vargas <davidvargas.tenerife@gmail.com>
2016-04-28 11:24:53 +0100

    N

--- check/idp1516/dominios/winclients-config.rb
+++ check/idp1516/dominios/winclients-config.rb
@@ -37,39 +37,39 @@ task "<wincli[12]> internal configurations" do
            { :label       => 'wincli1', 
              :goto        => :wincli1, 
              :ip          => :wincli1_ip, 
-             :short_name  => get(:wincli1_sname).upcase,
+             :sname       => get(:wincli1_sname).upcase,
              :productname => get(:wincli1_productname) } ,
            { :label       => 'wincli2', 
              :goto        => :wincli2, 
              :ip          => :wincli2_ip, 
-             :short_name  => get(:wincli2_sname).upcase.
+             :sname       => get(:wincli2_sname).upcase,
              :productname => get(:wincli2_productname) } 
          ]
          
   winclients.each do |cli|
     goto cli[:goto], :exec => "ipconfig /all"
     mac=result.find!("Direcci").content[0]
-    log ("#{cli[:label]}_MAC = #{mac}")
+    log    ("#{cli[:label]}_MAC = #{mac}")
     unique "MAC", mac
 
     target "#{cli[:label]} version"
-    goto cli[:goto], :exec => "ver"
+    goto   cli[:goto], :exec => "ver"
     expect result.find!("Windows").find!("6.1").count!.eq 1
 
     target "#{cli[:label]} COMPUTERNAME"
-    goto cli[:goto], :exec => "set"
-    expect result.find!("COMPUTERNAME").find!(cli[:short_name]).count!.eq 1
+    goto   cli[:goto], :exec => "set"
+    expect result.find!("COMPUTERNAME").find!(cli[:sname]).count!.eq 1
 
     target "#{cli[:label]} enlace <#{get(:gateway_ip)}>"
-    goto cli[:goto], :exec => "ipconfig"
+    goto   cli[:goto], :exec => "ipconfig"
     expect result.find!("enlace").find!(get(:gateway_ip)).count!.eq 1
 
     target "#{cli[:label]} router OK"
-    goto cli[:goto], :exec => "ping 8.8.4.4"
+    goto   cli[:goto], :exec => "ping 8.8.4.4"
     expect result.find!("Respuesta").count!.gt 1
 
     target "#{cli[:label]} DNS OK"
-    goto cli[:goto], :exec => "nslookup www.iespuertodelacruz.es"
+    goto   cli[:goto], :exec => "nslookup www.iespuertodelacruz.es"
     expect result.find!("Address:").find!("88.198.18.148").count!.eq 1
 
 #  target "Windows1 WORKGROUP_NAME"
@@ -77,7 +77,7 @@ task "<wincli[12]> internal configurations" do
 #  expect result.find!("Dominio de estaci").find!("de trabajo").find!(get(:domain).to_s.upcase).count!.eq 1
 
     target "#{cli[:label]} ProductName"
-    goto cli[:goto], :exec => "reg query \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\" /t REG_SZ"
+    goto   cli[:goto], :exec => "reg query \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\" /t REG_SZ"
     expect result.find!("ProductName").find!(cli[:productname]).count!.eq 1
   end
   
