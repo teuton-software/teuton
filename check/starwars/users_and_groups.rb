e502b0cd029648993b22861b6ddf4df1c87e8f4a
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-01 09:39:10 +0100

    F regexp use

--- check/starwars/users_and_groups.rb
+++ check/starwars/users_and_groups.rb
@@ -28,12 +28,12 @@ task "Checking Starwars Directories" do
 
   target "Directory <friends>"
   goto :host1, :exec => "vdir #{ "/home/"+get(:username) }"
-  expect result.grep!("drwxrwx---").grep!( get(:groupname) ).grep!("friends").count!.equal(1)
+  expect result.grep!(/^drwxrwx---/).grep!( get(:groupname) ).grep!("friends").count!.equal(1)
 
   result.restore!
   
   target "Directory <starwars>"
-  expect result.grep!("drwxrwxrwx").grep!( "users" ).grep!("starwars").count!.equal(1)
+  expect result.grep!(/^drwxrwxrwx/).grep!( "users" ).grep!(/starwars/).count!.equal(1)
 
 end
 
