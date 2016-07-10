38241b0143fec25fb40050be29cb420160d9a66e
David Vargas <davidvargas.tenerife@gmail.com>
2016-07-10 20:31:51 +0100

    U new version

--- lib/application.rb
+++ lib/application.rb
@@ -11,7 +11,7 @@ class Application
   
   def initialize
     @name="sysadmin-game"
-    @version="0.18.0"
+    @version="0.19.0"
     @letter={ :good=>'.', :bad=>'F', :error=>'?', :none=>' ' }
     @output_basedir="var"
    	@debug = false
