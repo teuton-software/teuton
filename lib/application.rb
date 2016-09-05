dbbacaa6edf3df4240369143e1e52d0e661dfa9b
David Vargas <davidvargas.tenerife@gmail.com>
2016-09-05 23:36:15 +0100

    U

--- lib/application.rb
+++ lib/application.rb
@@ -3,18 +3,18 @@ require 'singleton'
 
 class Application
   include Singleton
-    
+
   attr_reader   :name, :version, :letter, :output_basedir
   attr_reader   :debug, :verbose
   attr_accessor :global, :tasks, :hall_of_fame
-  
+
   def initialize
     @name="sysadmin-game"
     @version="0.20.0"
     @letter={ :good=>'.', :bad=>'F', :error=>'?', :none=>' ' }
     @output_basedir="var"
    	@debug = false
-	@verbose = true
+	  @verbose = true
 
     @global={}
     @tasks=[]
--- lib/case/dsl.rb
+++ lib/case/dsl.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-
 module DSL
 
   def target(pDescription=nil)
@@ -8,7 +7,7 @@ module DSL
     @action[:description]=pDescription
   end
   alias_method :desc, :target
-  	
+
   def command(pCommand, pArgs={})
     @action[:command]=pCommand
     tempfile(pArgs[:tempfile]) if pArgs[:tempfile]
@@ -23,7 +22,7 @@ module DSL
     #@running_config[key]=value
     @config.set(key,value)
   end
-  
+
   #Set weight value for the action
   def weight(pValue=nil)
     if pValue.nil? then
@@ -34,15 +33,15 @@ module DSL
       @action[:weight]=pValue.to_f
       end
   end
-	
+
   #Run command from the host identify as pHostname
-  #goto :host1, :execute => "command"   
+  #goto :host1, :execute => "command"
   def goto(pHostname=:localhost, pArgs={})
     @result.reset
     command(pArgs[:execute]) if pArgs[:execute]
     command(pArgs[:exec]) if pArgs[:exec]
     tempfile(pArgs[:tempfile]) if pArgs[:tempfile]
-	
+
     if pHostname==:localhost || pHostname=='localhost' || pHostname.to_s.include?('127.0.0.') then
       run_local_cmd
     else
@@ -70,11 +69,11 @@ module DSL
     @action[:weight]=lWeight
     @action[:check]=pCond
     @action[:result]=@result.value
-    
+
     @action[:alterations]=@result.alterations
     @action[:expected]=@result.expected
     @action[:expected]=pArgs[:expected] if pArgs[:expected]
-    
+
     @report.lines << @action.clone
     weight(1.0)
 
@@ -83,7 +82,7 @@ module DSL
     c=app.letter[:good] if pCond
     verbose c
   end
-	
+
   def log(pText="", pType=:info)
     s=""
     s=Rainbow("WARN!:").color(:yellow)+" " if pType==:warn
@@ -92,65 +91,65 @@ module DSL
   end
 
   alias_method :msg, :log
-	
+
   def unique( key, value )
     return if value.nil?
     k=(key.to_s+"="+value.to_s).to_sym
     @uniques << k
   end
-	
+
   def tempfile(pTempfile=nil)
     ext='.tmp'
     pre=@id.to_s+"-"
     if pTempfile.nil? then
       return @action[:tempfile]
-    elsif pTempfile==:default 
+    elsif pTempfile==:default
       @action[:tempfile]=File.join(@tmpdir, pre+'tt_local'+ext)
       @action[:remote_tempfile]=File.join(@remote_tmpdir, pre+'tt_remote'+ext)
     else
       @action[:tempfile]=File.join(@tmpdir, pre+pTempfile+ext)
       @action[:remote_tempfile]=File.join(@remote_tmpdir, pre+pTempfile+ext)
     end
-		
+
 	return @action[:tempfile]
   end
-  
+
   def tempdir
     @tmpdir
   end
 
-  def remote_tempfile		
+  def remote_tempfile
     return @action[:remote_tempfile]
   end
-  
+
   def remote_tempdir
     @remote_tmpdir
   end
 
   def send(pArgs={})
     return if get(:tt_skip)
-    
+
     format=@report.format
-    
+
     if pArgs[:copy_to] then
 
       host=pArgs[:copy_to].to_s
       ip=get((host+'_ip').to_sym)
       username=get((host+'_username').to_sym)
       password=get((host+'_password').to_sym)
-      
+
       filename="case-#{id_to_s}.#{format}"
       localfilepath=File.join(tempdir,"../out/",filename)
       if pArgs[:prefix]
         filename=pArgs[:prefix].to_s+filename
       end
-      
+
       if pArgs[:remote_dir]
-        remotefilepath=File.join(pArgs[:remote_dir],filename) 
+        remotefilepath=File.join(pArgs[:remote_dir],filename)
       else
         remotefilepath=File.join(remote_tempdir,filename)
       end
-           
+
       # upload a file or directory to the remote host
       begin
         Net::SFTP.start(ip, username, :password => password) do |sftp|
@@ -162,5 +161,5 @@ module DSL
       end
     end
   end
-  
+
 end
