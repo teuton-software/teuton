fdb10e9c1cebee37a6d13524846e6c2c6e634410
David Vargas <davidvargas.tenerife@gmail.com>
2016-09-28 23:10:09 +0100

    U user lib

--- check/idp/intro/start.rb
+++ check/idp/intro/start.rb
@@ -1,27 +1,6 @@
 
-=begin
-   Course name : ...
- Activity name : ...
-         MV OS : ...
-    Teacher OS : ...
-   English URL : ...
-   Spanish URL : ...
-=end
 
-task "Windows external configuration" do
-
-  goto   :localhost, :exec => "nmap -Pn #{get(:host1_ip)}" #Execute command once
-
-  ports=[ [ '23/tcp' , 'telnet'],
-          [ '139/tcp', 'netbios-ssn'] ]
-
-  ports.each do |port|
-    target "windows #{get(:host1_ip)} port #{port[0]}"
-    result.restore!
-    expect result.grep!(port[0]).grep!("open").grep!(port[1]).count!.eq(1)
-  end
-
-end
+require_relative 'windows'
 
 start do
   show
