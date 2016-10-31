5f715005bfe6093b0a8ea0d45cd20a0459078cca
David Vargas <davidvargas.tenerife@gmail.com>
2016-10-31 13:05:10 +0000

    U

--- check/idp/instalacion-dual/start.rb
+++ check/idp/instalacion-dual/start.rb
@@ -6,63 +6,69 @@
  MV OS       : Window7 con OpenSUSE 132.
 =end
 
+task "Preparativos" do
+  set( :firstname, get(:username))
+  set( :lastname1, get(:apellido1))
+  set( :lastname2, get(:apellido2))
+  set( :host2_ip , "172.19.#{get(:number).to_i.to_s}.32" )
+end
+
 task :hostname_configurations do
   #target "ping to <"+get(:host1_ip)+">"
   #goto :localhost, :exec => "ping #{get(:host1_ip)} | grep errors|wc -l"
   #expect result.eq(0)
 
-  target "SSH port 22 on <"+get(:host1_ip)+"> open"
-  goto :localhost, :exec => "nmap #{get(:host1_ip)} -Pn | grep ssh|wc -l"
+  target "SSH port 22 on <"+get(:host2_ip)+"> open"
+  goto :localhost, :exec => "nmap #{get(:host2_ip)} -Pn | grep ssh|wc -l"
   expect result.eq(1)
 
   _hostname="DUALX#{get(:lastname1)}"
   target "Checking hostname -a <"+_hostname+">"
-  goto :host1, :exec => "hostname -a"
+  goto :host2, :exec => "hostname -a"
   expect result.equal?(_hostname)
 
   _hostname="#{get(:lastname2)}"
   target "Checking hostname -d <"+_hostname+">"
-  goto :host1, :exec => "hostname -d"
+  goto :host2, :exec => "hostname -d"
   expect result.equal?(_hostname)
 
   _hostname="DUALX#{get(:lastname1)}.#{get(:lastname2)}"
   target "Checking hostname -f <"+_hostname+">"
-  goto :host1, :exec => "hostname -f"
+  goto :host2, :exec => "hostname -f"
   expect result.equal?(_hostname)
 
-  goto :host1, :exec => "blkid |grep sda1"
-  unique "UUID_sda1", result.value	
-  goto :host1, :exec => "blkid |grep sda2"
-  unique "UUID_sda2", result.value	
-  goto :host1, :exec => "blkid |grep sda6"
-  unique "UUID_sda6", result.value	
-  goto :host1, :exec => "blkid |grep sda7"
-  unique "UUID_sda7", result.value	
+  goto :host2, :exec => "blkid |grep sda1"
+  unique "UUID_sda1", result.value
+  goto :host2, :exec => "blkid |grep sda2"
+  unique "UUID_sda2", result.value
+  goto :host2, :exec => "blkid |grep sda6"
+  unique "UUID_sda6", result.value
+  goto :host2, :exec => "blkid |grep sda7"
+  unique "UUID_sda7", result.value
 end
 
 task :user_definitions do
   username=get(:firstname)
 
   target "User <#{username}> exists"
-  goto :host1, :exec => "cat /etc/passwd | grep '#{username}:' | wc -l"
+  goto :host2, :exec => "cat /etc/passwd | grep '#{username}:' | wc -l"
   expect result.eq(1)
 
   target "Users <#{username}> with not empty password "
-  goto :host1, :exec => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2| wc -l"
+  goto :host2, :exec => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2| wc -l"
   expect result.eq(1)
 
   target "User <#{username}> logged"
-  goto :host1, :exec => "last | grep #{username[0,8]} | wc -l"
+  goto :host2, :exec => "last | grep #{username[0,8]} | wc -l"
   expect result.neq(0)
 end
 
 task :disk_size do
   size='18'
   target "Disk sda size <#{size}>"
-  goto :host1, :exec => "lsblk |grep disk| grep sda| grep #{size}|grep G| wc -l"
+  goto :host2, :exec => "lsblk |grep disk| grep sda| grep #{size}|grep G| wc -l"
   expect result.eq(1)
 end
-
 
 task :partitions_size_and_type do
   partitions={ :sda1 => ['sda1'  ,'11,7G' , '12G'],
@@ -71,31 +77,32 @@ task :partitions_size_and_type do
                :sda6 => ['/home' ,'100M', '107M'],
                :sda7 => ['/'     ,'5G', '5,3G']
                 }
-  
+
   partitions.each_pair do |key,value|
     target "Partition #{key} mounted on <#{value[0]}>"
-    goto :host1, :exec => "lsblk |grep part| grep #{key}| grep #{value[0]}| wc -l"
+    goto :host2, :exec => "lsblk |grep part| grep #{key}| grep #{value[0]}| wc -l"
     expect result.eq(1)
 
     target "Partition #{key} size <#{value[1]}>"
-    goto :host1, :exec => "lsblk |grep part| grep #{key}| tr -s ' ' ':'| cut -d ':' -f 5"
-    expect(result.to_s.equal?(value[1]) || result.to_s.equal?(value[2]))    
+    goto :host2, :exec => "lsblk |grep part| grep #{key}| tr -s ' ' ':'| cut -d ':' -f 5"
+    expect(result.to_s.equal?(value[1]) || result.to_s.equal?(value[2]))
   end
 
   partitions={ :sda6 => ['/dev/disk', '/home', 'ext3'],
-               :sda7 => ['/dev/disk', '/'    , 'ext4'] 
+               :sda7 => ['/dev/disk', '/'    , 'ext4']
              }
-  
-  partitions.each_pair do |key,value|  
+
+  partitions.each_pair do |key,value|
      target "Partition #{key} type <#{value[2]}>"
-    goto :host1, :exec => "df -hT | grep #{key}| grep #{value[2]}|wc -l"
+    goto :host2, :exec => "df -hT | grep #{key}| grep #{value[2]}|wc -l"
     expect result.eq(1)
-  end  
+  end
 end
 
 start do
-	show :resume
-	export :all
+	show
+	export :format => :colored_text
+  send :copy_to => :host2
 end
 
 =begin
