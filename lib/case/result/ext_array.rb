6e26ad019dc528e9f8f5f19786373aace1785b6a
David Vargas <davidvargas.tenerife@gmail.com>
2016-12-13 18:20:33 +0000

    U split Result class

---
+++ lib/case/result/ext_array.rb
@@ -0,0 +1,42 @@
+
+
+# This is an extension of Result class
+class Result
+  def count!
+    @alterations << 'count!'
+
+    if @content.class == Array
+      @content = [@content.count]
+    elsif @content.nil?
+      @content = ['0']
+    else
+      @content = [@content.to_i.to_s]
+    end
+
+    self
+  end
+
+  def include?(p_value)
+    @expected = "Include <#{p_value}> value"
+    @content[0].include?(p_value)
+  end
+
+  def not_include?(p_value)
+    @expected = "Not include <#{p_value}> value"
+    !@content[0].include?(p_value)
+  end
+
+  def contain?(p_value)
+    @expected = "Contain <#{p_value}> value"
+    @content.contain? p_value
+  end
+
+  def empty
+    @expected = 'Empty!'
+    @content.empty
+  end
+
+  alias length!    count!
+  alias size!      count!
+  alias empty?     empty
+end
