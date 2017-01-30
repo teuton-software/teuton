fc9cf182dda1ba4ab88489e7448af2c2ff740f34
David Vargas <davidvargas.tenerife@gmail.com>
2017-01-30 08:05:57 +0000

    U rename file

--- check/add/puppet/start.rb
+++ check/add/puppet/start.rb
@@ -4,9 +4,10 @@ Spanish Exercise URL:
 * https://github.com/dvarrui/libro-de-actividades/blob/master/actividades/add/devops/puppet-opensuse.md
 =end
 
+require_relative 'master-hostconf'
+
 #require_relative 'windows'
 #require_relative 'windows-server'
-require_relative 'opensuse'
 #require_relative 'debian'
 
 start do
