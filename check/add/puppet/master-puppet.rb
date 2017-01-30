6c64a109a01a7b8efd19cbf58c9374b2edce5049
David Vargas <davidvargas.tenerife@gmail.com>
2017-01-30 19:05:32 +0000

    U master targets finish

--- check/add/puppet/master-puppet.rb
+++ check/add/puppet/master-puppet.rb
@@ -1,35 +1,43 @@
 
-
 task "Master check hostnames" do
   set :client1_ip, "172.18." + get(:number).to_i.to_s + ".101"
   set :client2_ip, "172.18." + get(:number).to_i.to_s + ".102"
 
   goto :master, :exec => "cat /etc/hosts"
 
-  target "Master conf into etc hosts file"
+  target "Master: master info into /etc/hosts file"
   result.restore!
-  expect result.find!(get(:master_ip)).find!('master').eq(1)
+  expect result.find!(get(:master_ip)).find!('master').find!(get(:master_domain)).eq(1)
 
-  target "Master conf into etc hosts file"
+  target "Master: client1 info into /etc/hosts file"
   result.restore!
-  expect result.find!(get(:client1_ip)).find!('client1').eq(1)
+  expect result.find!(get(:client1_ip)).find!('client1').find!(get(:client1_domain)).eq(1)
 
-  target "Master conf into etc hosts file"
+  target "Master: client2 into /etc/hosts file"
   result.restore!
-  expect result.find!(get(:client2_ip)).find!('client2').eq(1)
+  expect result.find!(get(:client2_ip)).find!('client2').find!(get(:client2_domain)).eq(1)
 end
 
-task "Master installed software" do
-  packages = ['rubygems-puppet-master', 'rubygems-puppet']
+task "Master software" do
+  packages = ['rubygem-puppet-master', 'rubygem-puppet']
 
-  packages.each do |package|
-    target "<" + package + "> installed"
-    goto :master, :exec => "zypper se #{package}"
-    expect result.find!('ii ').find!(package).ge(1)
+  packages.each do |packagename|
+    target "<" + packagename + "> installed"
+    goto :master, :exec => "zypper se #{packagename}"
+    expect result.find!('ii ').find!(packagename).ge(1)
   end
+
+  goto :master, :exec => "systemctl status puppetmaster"
+  target "Service <puppetmaster> active"
+  result.restore!
+  expect result.find!('Active: ').find!('running').eq(1)
+
+  target "Service <puppetmaster> enable"
+  result.restore!
+  expect result.find!('Loaded: ').find!(' enable').eq(1)
 end
 
-task "Master manifest files" do
+task "Master: puppet files" do
   target "<site.pp> created"
   goto :master, :exec => "file /etc/puppet/manifests/site.pp"
   expect result.find!('ASCII text ').eq(1)
@@ -41,5 +49,16 @@ task "Master manifest files" do
     goto :master, :exec => "file /etc/puppet/manifests/classes/#{filename}"
     expect result.find!('ASCII text ').eq(1)
   end
+end
 
+task "Master: certificates" do
+  certname = "cli1alu" + get(:number).to_s + '.' + get(:client1_domain)
+  target "Certificates <#{certname}> accepted"
+  goto :master, :exec => "puppet cert print #{certname}"
+  expect result.count!.neq(0)
+
+  certname = 'cli2alu' + get(:number)
+  target "Certificates <#{certname}> accepted"
+  goto :master, :exec => "puppet cert print #{certname}"
+  expect result.count!.neq(0)
 end
