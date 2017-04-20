754be3c45b05c2332a2f980b70288c8bce9954c2
David Vargas <davidvargas.tenerife@gmail.com>
2017-04-20 08:21:06 +0100

    U

--- check/idp/dominios/start.rb
+++ check/idp/dominios/start.rb
@@ -1,15 +1,15 @@
 
 =begin
-Spanish URL: 
+Spanish URL:
 * https://github.com/dvarrui/libro-de-actividades/blob/master/actividades/idp/dominios/pdc-winserver.md
 =end
 
-require_relative 'winserver-config' 
-require_relative 'winclients-config' 
-require_relative 'winserver-active-directory' 
+require_relative 'winserver-config'
+require_relative 'winclients-config'
+require_relative 'winserver-active-directory'
 
 start do
   show
-  #export :format => :colored_text
-  export
+  export :format => :colored_text
+  #export
 end
