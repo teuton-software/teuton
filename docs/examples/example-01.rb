c7c5bcf9f9330477bcb756962c76abd8964c29f7
David Vargas <davidvargas.tenerife@gmail.com>
2017-05-20 12:17:20 +0100

    F

--- docs/examples/example-01.rb
+++ docs/examples/example-01.rb
@@ -4,7 +4,7 @@
   * target : Describe the target
   * goto   : Move to localhost, and execute the command
   * expect : Check if the results are equal to expected value
-  
+
   Teacher host (localhost) must have GNU/Linux OS.
 =end
 
@@ -18,16 +18,16 @@ task "Create user obiwan" do
   goto :localhost, :exec => "id obiwan"
   expect result.count!.eq 1
 
-  target "Checking user <obiwan, obi-wan> using find! and count! methods with String arg"
+  target "Checking user <obiwan> using find! and count! methods with String arg"
   goto :localhost, :exec => "cat /etc/passwd"
   expect result.find!("obiwan").count!.eq 1
 
-  target "Checking user <obiwan> using find! and count! methods with Regexp arg"
+  target "Checking user <obiwan, obi-wan> using find! and count! methods with Regexp arg"
   goto :localhost, :exec => "cat /etc/passwd"
   expect result.find!(/obiwan|obi-wan/).count!.eq 1
 end
 
 start do
   show
-  export 
+  export
 end
