8c1a507fcbb3dcdb484385fc4f37853e80f10ebf
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-08 20:41:42 +0100

    U new test

--- tests/project_test.rb
+++ tests/project_test.rb
@@ -14,12 +14,6 @@ class ProjectTest < Minitest::Test
     assert_equal 'tests/files/example-01.yaml', config_path
     assert_equal 'example-01', test_name
 
-    # Simple mode, files exists
-    script_path, config_path, test_name = Project.find_filenames_for('tests/files/example-04.rb')
-    assert_equal 'tests/files/example-04.rb', script_path
-    assert_equal 'tests/files/example-04.json', config_path
-    assert_equal 'example-04', test_name
-
     # Complex mode, dir empty
     script_path, config_path, test_name = Project.find_filenames_for('tests/files/example-02')
     assert_equal 'tests/files/example-02/start.rb', script_path
@@ -31,6 +25,18 @@ class ProjectTest < Minitest::Test
     assert_equal 'tests/files/example-03/start.rb', script_path
     assert_equal 'tests/files/example-03/config.yaml', config_path
     assert_equal 'example-03', test_name
+
+    # Simple mode, files exists with JSON
+    script_path, config_path, test_name = Project.find_filenames_for('tests/files/example-04.rb')
+    assert_equal 'tests/files/example-04.rb', script_path
+    assert_equal 'tests/files/example-04.json', config_path
+    assert_equal 'example-04', test_name
+
+    # Complex mode, files exists with JSON
+    script_path, config_path, test_name = Project.find_filenames_for('tests/files/example-05')
+    assert_equal 'tests/files/example-05/start.rb', script_path
+    assert_equal 'tests/files/example-05/config.json', config_path
+    assert_equal 'example-05', test_name
     Application.instance.verbose = true
   end
 end
