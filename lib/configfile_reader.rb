b134832328ad933cbc2161be882ae005ba528e98
David Vargas <davidvargas.tenerife@gmail.com>
2017-06-09 13:47:20 +0100

    U loading json files

--- lib/configfile_reader.rb
+++ lib/configfile_reader.rb
@@ -4,8 +4,14 @@ require 'json'
 
 # Functions that read dato from ConfigFile using formats YAML and JSON
 module ConfigFileReader
-  def self.read(configfile_pathname)
-    data = YAML.load(File.open(configfile_pathname))
-    data
+  def self.read(filepath)
+    if File.extname(filepath) == '.yaml'
+      data = YAML.load(File.open(filepath))
+      return data
+    elsif File.extname(filepath) == '.json'
+      data = JSON.parse(File.read(filepath), :symbolize_names => true)
+      return data
+    end
+    raise "[ERROR] #{filepath} dosn't exist!"
   end
 end
--- tests/configfile_reader_test.rb
+++ tests/configfile_reader_test.rb
@@ -9,5 +9,19 @@ class ConfigFileReaderTest < Minitest::Test
     data = ConfigFileReader.read('tests/files/example-01.yaml')
     assert_equal nil, data[:global]
     assert_equal 1, data[:cases].size
+    assert_equal Hash, data[:cases][0].class
+    assert_equal 'student1', data[:cases][0][:tt_members]
+
+    data = ConfigFileReader.read('tests/files/example-03/config.yaml')
+    assert_equal nil, data[:global]
+    assert_equal 1, data[:cases].size
+    assert_equal Hash, data[:cases][0].class
+    assert_equal 'student1', data[:cases][0][:tt_members]
+
+    data = ConfigFileReader.read('tests/files/example-04.json')
+    assert_equal nil, data[:global]
+    assert_equal 1, data[:cases].size
+    assert_equal Hash, data[:cases][0].class
+    assert_equal 'student1', data[:cases][0][:tt_members]
   end
 end
