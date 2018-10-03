e6a13d9b5e50d4d11e6d89a53c70165ccc202214
David <dvarrui@gmail.com>
2018-10-04 00:15:08 +0100

    U

--- check/test/test-connection/connection.rb
+++ check/test/test-connection/connection.rb
@@ -1,10 +1,10 @@
 
 task "GNU/Linux connections" do
 
-  set(:host1_ip, get(:host_ip))
-  set(:host2_ip, get(:host_ip))
-  set(:host3_ip, get(:host_ip))
-  set(:host4_ip, get(:host_ip))
+  set(:host1_ip, get(:linux_ip))
+  set(:host2_ip, get(:linux_ip))
+  set(:host3_ip, get(:linux_ip))
+  set(:host4_ip, get(:linux_ip))
 
   target "host1 SSH with <#{get(:host1_username)}> "
   goto   :host1, :exec => "ip a"
@@ -21,5 +21,30 @@ task "GNU/Linux connections" do
   target "host4 Telnet with <#{get(:host4_username)}> "
   goto   :host4, :exec => "ip a"
   expect result.find!(get(:host4_ip)).count!.eq 1
-  
+
+end
+
+task "Windows connections" do
+
+  set(:win1_ip, get(:windows_ip))
+  set(:win2_ip, get(:windows_ip))
+  set(:win3_ip, get(:windows_ip))
+  set(:win4_ip, get(:windows_ip))
+
+  target "win1 SSH with <#{get(:win1_username)}> "
+  goto   :win1, :exec => "whoami"
+  expect result.find!(get(:win1_username)).count!.eq 1
+
+  target "win2 SSH with <#{get(:win2_username)}> "
+  goto   :win2, :exec => "whoami"
+  expect result.find!(get(:win2_username)).count!.eq 1
+
+  target "win3 Telnet with <#{get(:win3_username)}> "
+  goto   :win3, :exec => "whoami"
+  expect result.find!(get(:win3_username)).count!.eq 1
+
+  target "win4 Telnet with <#{get(:win4_username)}> "
+  goto   :win4, :exec => "whoami"
+  expect result.find!(get(:win4_username)).count!.eq 1
+
 end
