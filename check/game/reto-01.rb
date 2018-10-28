5a6b30bd090219c169ea5b331fdec1237bef6f26
David <dvarrui@gmail.com>
2018-10-28 17:04:20 +0000

    N new retos

--- check/game/reto-01.rb
+++ check/game/reto-01.rb
@@ -1,16 +1,9 @@
 # encoding: utf-8
 
-=begin
- Name        : target 01
- Author      : David Vargas Ruiz
- Description : Create user with your name
-=end
-
-
 task "Target 01" do
 
   target "Create user <"+get(:username)+">"
-  goto :localhost, :exec => "id #{get(:username)}"
+  run "id #{get(:username)}"
   expect result.count!.equal?(1)
 
 end
--- check/game/reto-03.rb
+++ check/game/reto-03.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-task "Target 02" do
+task "Target 03" do
 
   target "Create user <"+get(:username)+">"
   run "id #{get(:username)}"
@@ -10,6 +10,11 @@ task "Target 02" do
   result.restore!
   expect result.grep!(get(:groupname)).count!.equal?(1)
 
+  home = "/home/" + get(:username)
+
+  target "User home is <" + home + ">"
+  run "cat /etc/password"
+  expect result.grep!(home).grep!(get(:username)).count!.equal?(1)
 end
 
 start do
