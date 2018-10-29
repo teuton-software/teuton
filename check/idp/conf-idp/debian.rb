8c0ba5235b3d6357b697b534be0c15085d2ae54d
David <dvarrui@gmail.com>
2018-10-29 09:30:42 +0000

    U

--- check/idp/conf-idp/debian.rb
+++ check/idp/conf-idp/debian.rb
@@ -8,7 +8,7 @@
 
 task 'General' do
   target "Checking SSH port <"+get(:host1_ip)+">"
-  goto  :localhost, :exec => "nmap -Pn #{get(:host1_ip)}"
+  run "nmap -Pn #{get(:host1_ip)}"
   expect result.find!('ssh').find!('open').count!.eq(1)
 end
 
