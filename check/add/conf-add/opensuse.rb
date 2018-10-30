7e5bc21964e4d769032ade90173f0f79892a5949
David <dvarrui@gmail.com>
2018-10-30 12:09:43 +0000

    U

--- check/add/conf-add/opensuse.rb
+++ check/add/conf-add/opensuse.rb
@@ -5,6 +5,7 @@ task 'General' do
   run "nmap -Pn #{get(:host1_ip)}"
   expect result.find!('ssh').find!('open').count!.eq(1)
 
+  goto  :host1, :exec => "hostname"
   unique "hostname", result.value
   goto  :host1, :exec => "blkid |grep sda1"
   unique "UUID", result.value
--- check/add/conf-add/ssh.rb
+++ check/add/conf-add/ssh.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
 task 'hostname_configurations' do
-  my_hostname="ssh-server#{get(:number)}g.#{get(:dominio)}"
 
+  my_hostname="ssh-server#{get(:number)}"
   target "Checking hostname <"+my_hostname+">"
-  goto  :host1, :exec => "hostname -f"
+  goto  :host1, :exec => "hostname"
   expect result.equal?(my_hostname)
 
 end
