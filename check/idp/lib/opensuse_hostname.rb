9b47e8430fc59acf145554388bdb4014f8bcb9bf
David <dvarrui@gmail.com>
2018-11-05 10:37:30 +0000

    F

--- check/idp/lib/opensuse_hostname.rb
+++ check/idp/lib/opensuse_hostname.rb
@@ -1,12 +1,12 @@
 
 task 'Opensuse HOSTNAME configurations' do
-  my_hostname="#{get(:lastname1)}#{get(:number)}g.#{get(:dominio)}1"
 
-  target "Checking hostname <"+my_hostname+">"
+  target "Checking hostname <" + get(:suse1_hostname) + ">"
   goto  :suse1, :exec => "hostname -f"
-  expect result.equal?(my_hostname)
+  expect result.equal?(get(:suse1_hostname))
 
   unique "hostname", result.value
   goto  :suse1, :exec => "blkid |grep sda1"
   unique "UUID", result.value
+
 end
