a2b327a93cb20d62a42fe03be948f405b985d9d1
David <dvarrui@gmail.com>
2018-11-05 10:27:09 +0000

    U

--- check/idp/lib/gnulinux_user.rb
+++ check/idp/lib/gnulinux_user.rb
@@ -4,14 +4,14 @@ task 'user_definitions' do
   username=get(:firstname)
 
   target "User <#{username}> exists"
-  goto  :host1, :exec => "cat /etc/passwd"
+  goto  :linux1, :exec => "cat /etc/passwd"
   expect result.find!(username).count!.eq(1)
 
   target "Users <#{username}> with not empty password "
-  goto  :host1, :exec => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2"
+  goto  :linux1, :exec => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2"
   expect result.count!.eq(1)
 
   target "User <#{username}> logged"
-  goto  :host1, :exec => "last | grep #{username[0,8]}"
+  goto  :linux1, :exec => "last | grep #{username[0,8]}"
   expect result.count!.neq(0)
 end
