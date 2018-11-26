669ed5664f00f334ff3556d5252d92c35b3bf2e0
David <dvarrui@gmail.com>
2018-11-26 00:57:07 +0000

    U

--- clients/debian-ssh.sh
+++ clients/debian-ssh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
- echo "[INFO] SSH on Debian"
- echo "[INFO] Installation..."
- apt in -y ssh-server
+CONFIGFILE="/etc/ssh/sshd_config"
+BACKUPFILE="$CONFIGFILE.bak"
+
+echo "[INFO] SSH on Debian"
+echo "[INFO] Installation..."
+apt in -y openssh-server
+systemctl enable openssh-server
+
+echo "[INFO] Configuration..."
+
+cp $CONFIGFILE $BACKUPFILE
+sed 's/^#PermitRootLogin yes/PermitRootLogin yes/g' $BACKUPFILE > $CONFIGFILE
+systemctl restart openssh-server
--- clients/opensuse-ssh.sh
+++ clients/opensuse-ssh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
- echo "[INFO] SSH on OpenSUSE"
- echo "[INFO] Installation..."
- zypper in -y openssh
+CONFIGFILE="/etc/ssh/sshd_config"
+BACKUPFILE="$CONFIGFILE.bak"
+
+echo "[INFO] SSH on OpenSUSE"
+echo "[INFO] Installation..."
+zypper in -y openssh
+systemctl enable sshd
+
+echo "[INFO] Configuration..."
+
+cp $CONFIGFILE $BACKUPFILE
+sed 's/^#PermitRootLogin yes/PermitRootLogin yes/g' $BACKUPFILE > $CONFIGFILE
+systemctl restart sshd
