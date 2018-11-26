a7811dc6a098fea20b24e06305cc214efcb99f0b
David <dvarrui@gmail.com>
2018-11-26 00:23:58 +0000

    U

--- lib/laboratory.rb
+++ lib/laboratory.rb
@@ -3,7 +3,7 @@ require 'terminal-table'
 require_relative 'application'
 require_relative 'configfile_reader'
 require_relative 'case/result'
-require 'pry-byebug'
+#require 'pry-byebug'
 
 def task(name, &block)
   Application.instance.tasks << { name: name, block: block }
