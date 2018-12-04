a7d7c8de4c6d9d49e9c89c91201722e0e924b4d9
David <dvarrui@gmail.com>
2018-12-04 21:15:05 +0000

    U

--- challenges/against-my-self/level-02.rb
+++ challenges/against-my-self/level-02.rb
@@ -3,10 +3,12 @@
 task "Target 02" do
 
   target "Create user <"+get(:username)+">"
+  request "Debes crear el usuario <NOMBREALUMNO>"
   run "id #{get(:username)}"
   expect result.count!.equal?(1), :weight => 2.0
 
   target "Member of group <"+get(:groupname)+">"
+  request "Además el usuario creado debe ser miembro del grupo VBoxUsers"
   run "id #{get(:username)}"
   expect result.find!(get(:groupname)).count!.equal?(1)
 
