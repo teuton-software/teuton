ec26a59d2170a182c9f640914224a0551ce7fa9d
David <dvarrui@gmail.com>
2018-12-05 02:28:03 +0000

    U

--- challenges/against-my-self/level-03.rb
+++ challenges/against-my-self/level-03.rb
@@ -6,16 +6,19 @@ task "Target 03" do
   groupname = "users"
 
   target "Create user <"+ username+">"
+  request "Must create user <#{username}>"
   run "id #{username}"
   expect result.count!.equal?(1)
 
   target "Member of group <"+ groupname+">"
+  request "User <#{username}> must be member of <#{groupname}>"
   result.restore!
   expect result.grep!(groupname).count!.equal?(1)
 
   home = "/home/" + username
 
   target "User home is <" + home + ">"
+  request "User <#{username}> must have home directory as <#{home}>"
   run "cat /etc/passwd"
   expect result.grep!(home).grep!(username).count!.equal?(1)
 end
