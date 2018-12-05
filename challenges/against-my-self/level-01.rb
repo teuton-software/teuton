b1e6287b553aad50addb9f6dae8e8f11387bab39
David <dvarrui@gmail.com>
2018-12-05 02:18:42 +0000

    U

--- challenges/against-my-self/level-01.rb
+++ challenges/against-my-self/level-01.rb
@@ -3,6 +3,7 @@
 task "Target 01" do
 
   target "Create user <" + get(:username) + ">"
+  request "You have to create a new user STUDENTNAME."
   run "id " + get(:username)
   expect result.count.equal?(1)
 
