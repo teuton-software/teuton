c83a5e9e311aca8fb105383985b55995ab1709d5
David <dvarrui@protonmail.com>
2019-01-03 22:57:35 +0000

    F

--- challenges/add/nfs/nfs.rb
+++ challenges/add/nfs/nfs.rb
@@ -1,12 +1,12 @@
 task "Settings" do
   username = get(:username)
   password = get(:password)
-  set(:mv1_username,:username)
-  set(:mv2_username,:username)
-  set(:mv3_username,:username)
-  set(:mv1_password,:password)
-  set(:mv2_password,:password)
-  set(:mv3_password,:password)
+  set(:mv1_username, username)
+  set(:mv2_username, username)
+  set(:mv3_username, username)
+  set(:mv1_password, password)
+  set(:mv2_password, password)
+  set(:mv3_password, password)
 end
 
 task "NFS" do
