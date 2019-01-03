c5e8e4d690fc372c1f45384b53cffbb72056d4bb
David <dvarrui@protonmail.com>
2019-01-03 23:11:51 +0000

    U

--- challenges/add/nfs/nfs.rb
+++ challenges/add/nfs/nfs.rb
@@ -15,12 +15,13 @@ task "NFS" do
 
   target "Instalar el servicio NFS en MV1"
   goto :mv1, :exec => "systemctl status nfs-kernel-server"
-  goto :mv1, :exec => "echo $?"
-  expect result.grep("0").equal(1)
+#  goto :mv1, :exec => "echo \$?"
+#  result.debug
+  expect result.grep("active").count.equal(1)
 
 end
 
 start do
   show    # esto es para mostrar los resultados en consola
-#  export
+  export
 end
