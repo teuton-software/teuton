e1a4911b2c834a028f225f60a1d921fc09ff6970
David <dvarrui@protonmail.com>
2019-01-04 00:55:04 +0000

    U create <group> as <task> alias

--- lib/sysadmingame.rb
+++ lib/sysadmingame.rb
@@ -6,6 +6,10 @@ def task(name, &block)
   Application.instance.tasks << { name: name, block: block }
 end
 
+def group(name, &block)
+  task(name, &block)
+end
+
 def start(&block)
   Tool.instance.start(&block)
 end
