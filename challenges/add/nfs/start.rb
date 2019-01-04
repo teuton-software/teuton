100ec7587e10bf87f05cef1c73e371362f540699
David <dvarrui@protonmail.com>
2019-01-04 00:19:33 +0000

    U

--- challenges/add/nfs/start.rb
+++ challenges/add/nfs/start.rb
@@ -1,29 +1,30 @@
 task "Settings" do
-
   username = get(:username)
   password = get(:password)
-  set(:mv1_username, username)
-  set(:mv2_username, username)
-  set(:mv3_username, username)
-  set(:mv1_password, password)
-  set(:mv2_password, password)
-  set(:mv3_password, password)
 
+  set(:mv1_username, username)
+  set(:mv1_password, password)
+  hostname = get(:hostname1)+get(:number)
+  set(:mv1_hostname, hostname)
+
+  set(:mv2_username, username)
+  set(:mv2_password, password)
+  hostname = get(:hostname2)+get(:number)
+  set(:mv2_hostname, hostname)
+
+  set(:mv3_username, username)
+  set(:mv3_password, password)
+  hostname = get(:hostname3)+get(:number)
+  set(:mv3_hostname, hostname)
 end
 
-task "NFS" do
-
-  target "Usuario <add>"
-  goto :mv1, :exec => "id add"
-  expect result.grep!("sudo").count!.equal(1)
-
-  target "Instalar el servicio NFS en MV1"
+task "NFS Activity" do
+  target "Servicio NFS activo en la MV1"
   goto :mv1, :exec => "systemctl status nfs-kernel-server"
   expect result.grep!("active").count!.equal(1)
-
 end
 
 start do
-  show    # esto es para mostrar los resultados en consola
-  export
+  show    # Muestra el resumen de resultados en el terminal
+  export  # Generar ficheros de salida detallados
 end
