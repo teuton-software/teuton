5deea0349d5fe6ffb57cc672ce9223851246d902
David <dvarrui@protonmail.com>
2019-01-04 00:24:44 +0000

    U new major version 1.8.0. Thanks Fran!

--- lib/application.rb
+++ lib/application.rb
@@ -13,7 +13,7 @@ class Application
 
   def initialize
     @name = 'teuton'
-    @version = '1.7.0'
+    @version = '1.8.0'
     @letter = { good: '.', bad: 'F', error: '?', none: ' ' }
     @output_basedir = 'var'
     @debug = false
