0e99195998b7462bfd40836221a2ef1184bd62ae
David <dvarrui@protonmail.com>
2019-01-14 18:54:52 +0000

    N play

---
+++ lib/command/play.rb
@@ -0,0 +1,20 @@
+
+class ProjectCLI < Thor
+
+  map ['p', '-p', 'play', '--play'] => 'play'
+  desc 'play PATH/TO/FILE/FOO.rb', 'Run activity script file'
+  long_desc <<-LONGDESC
+
+  SIMPLE MODE: Run challenge from <challenges/demo/foo.rb>.
+
+  Example: #{$PROGRAM_NAME} challenges/demo/foo.rb
+
+  COMPLEX MODE: Run challenge from <challenges/demo/foo/start.rb>.
+
+  Example: #{$PROGRAM_NAME} challenges/demo/foo
+
+LONGDESC
+  def play(path_to_rb_file)
+    Project.run(path_to_rb_file)
+  end
+end
