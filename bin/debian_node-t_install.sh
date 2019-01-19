3f8fabe9d1704f61670063b471ac48bc56132f4a
David <dvarrui@protonmail.com>
2019-01-19 19:39:25 +0000

    U

--- bin/debian_node-t_install.sh
+++ bin/debian_node-t_install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-CONFIGFILE="/etc/ssh/sshd_config"
-BACKUPFILE="$CONFIGFILE.bak"
-
-echo "[INFO] Debian S-NODE installation"
-echo "[INFO] Installation..."
-apt in -y openssh-server
+echo "[INFO] Debian T-NODE installation"
+echo "[INFO] Packages..."
+apt in -y git
 systemctl enable openssh-server
 
 echo "[INFO] Configuration..."
