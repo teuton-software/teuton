71ba992b7e7799e9a6adb30194aff8317e7a297b
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-01-22 14:23:01 +0000

    Update windows_t-node_install.bat

--- bin/windows_t-node_install.bat
+++ bin/windows_t-node_install.bat
@@ -1,10 +1,28 @@
+@echo off
+
 REM Windows T-NODE installation
-REM version: 20190121
+REM version: 20190122
 
+echo [INFO] WINDOWS T-NODE installation
+echo [INFO] Installing PACKAGES...
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install -y git
+choco install -y ruby
+
+REM Update environment variables after packages installation
+call refreshenv
+
+REM Rake Gem installation
+call gem install rake -f
+
+echo [INFO] Cloning git REPO...
 git clone https://github.com/dvarrui/teuton.git
-gem install rake
 
+echo [INFO] Checking...
 cd teuton
-rake gems
-rake get_challenges
-rake
+call rake gems
+call rake get_challenges
+call rake
+
+echo [INFO] Finish!
+ruby teuton version
