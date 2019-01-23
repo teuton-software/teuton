ef176be5968b7ee9b995cb072155591ca8b7a299
David <dvarrui@protonmail.com>
2019-01-23 07:48:36 +0000

    U proxy-cache test run on localhost. Installing spanish doc fixed. Added Windows installer

--- lib/application.rb
+++ lib/application.rb
@@ -13,7 +13,7 @@ class Application
 
   def initialize
     @name = 'teuton'
-    @version = '1.11.0'
+    @version = '1.12.0'
     @letter = { good: '.', bad: 'F', error: '?', none: ' ' }
     @output_basedir = 'var'
     @debug = false
