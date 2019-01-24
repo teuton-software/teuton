80b396c9348b7b9998a42b56bec0bcd7c9e0a331
David <dvarrui@protonmail.com>
2019-01-24 19:43:43 +0000

    F teuton use absolute path as file references instead of relative ones

--- tests/project_test.rb
+++ tests/project_test.rb
@@ -10,33 +10,46 @@ class ProjectTest < Minitest::Test
     app = Application.instance
     app.verbose = false
     # Simple mode, files exists
+    app.running_basedir = Dir.getwd
+    basedir = app.running_basedir
+
     Project.find_filenames_for('tests/files/example-01.rb')
-    assert_equal 'tests/files/example-01.rb', app.script_path
-    assert_equal 'tests/files/example-01.yaml', app.config_path
+    a = File.join(basedir,'tests/files/example-01.rb')
+    b = File.join(basedir,'tests/files/example-01.yaml')
+    assert_equal a, app.script_path
+    assert_equal b, app.config_path
     assert_equal 'example-01', app.test_name
 
     # Complex mode, dir empty
     Project.find_filenames_for('tests/files/example-02')
-    assert_equal 'tests/files/example-02/start.rb', app.script_path
-    assert_equal 'tests/files/example-02/config.yaml', app.config_path
+    a = File.join(basedir,'tests/files/example-02/start.rb')
+    b = File.join(basedir,'tests/files/example-02/config.yaml')
+    assert_equal a, app.script_path
+    assert_equal b, app.config_path
     assert_equal 'example-02', app.test_name
 
     # Complex mode, files exists
     Project.find_filenames_for('tests/files/example-03')
-    assert_equal 'tests/files/example-03/start.rb', app.script_path
-    assert_equal 'tests/files/example-03/config.yaml', app.config_path
+    a = File.join(basedir,'tests/files/example-03/start.rb')
+    b = File.join(basedir,'tests/files/example-03/config.yaml')
+    assert_equal a, app.script_path
+    assert_equal b, app.config_path
     assert_equal 'example-03', app.test_name
 
     # Simple mode, files exists with JSON
     Project.find_filenames_for('tests/files/example-04.rb')
-    assert_equal 'tests/files/example-04.rb', app.script_path
-    assert_equal 'tests/files/example-04.json', app.config_path
+    a = File.join(basedir,'tests/files/example-04.rb')
+    b = File.join(basedir,'tests/files/example-04.json')
+    assert_equal a, app.script_path
+    assert_equal b, app.config_path
     assert_equal 'example-04', app.test_name
 
     # Complex mode, files exists with JSON
     Project.find_filenames_for('tests/files/example-05')
-    assert_equal 'tests/files/example-05/start.rb', app.script_path
-    assert_equal 'tests/files/example-05/config.json', app.config_path
+    a = File.join(basedir,'tests/files/example-05/start.rb')
+    b = File.join(basedir,'tests/files/example-05/config.json')
+    assert_equal a, app.script_path
+    assert_equal b, app.config_path
     assert_equal 'example-05', app.test_name
     app.verbose = true
   end
