f4c073efa200a4091d995f3e27e23916f4ca2e81
David <dvarrui@protonmail.com>
2019-01-28 22:54:44 +0000

    F ignore

---
+++ lib/command/challenges.rb
@@ -0,0 +1,20 @@
+
+class Teuton < Thor
+
+  map ['-dc', '--dc', '--download'] => 'download'
+  desc 'download', 'Download Teuton challenges from git repo'
+  long_desc  <<-LONGDESC
+  Download Teuton challenges from git repo.
+  You can do <git clone https://github.com/dvarrui/teuton-challenges.git> manually, or use this option.
+
+  Example:
+
+  #{$PROGRAM_NAME} download_challenges
+
+LONGDESC
+  def download
+    puts "[INFO] Downloading <teuton-challenges> repo..."
+#    system("cd #{current_dir} && git clone https://github.com/dvarrui/teuton-challenges.git")
+    system("git clone https://github.com/dvarrui/teuton-challenges.git")
+  end
+end
