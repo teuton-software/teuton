36965eabf6a58c15203cf486b30b12d9d1f19b07
David <dvarrui@protonmail.com>
2019-01-30 07:32:27 +0000

    U working on JSON and YAML output formats

--- lib/formatter/formatter_factory.rb
+++ lib/formatter/formatter_factory.rb
@@ -1,27 +1,30 @@
 # encoding: utf-8
 
+require_relative 'colored_text_formatter'
 require_relative 'csv_formatter'
 require_relative 'html_formatter'
 require_relative 'txt_formatter'
-require_relative 'colored_text_formatter'
+require_relative 'yaml_formatter'
 require_relative 'xml_formatter'
 
 module FormatterFactory
-	
+
   def FormatterFactory.get(pReport, pFormat, pFilename)
     case pFormat
+		when :colored_text
+      f=ColoredTextFormatter.new(pReport)
     when :csv
       f=CSVFormatter.new(pReport)
     when :html
       f=HTMLFormatter.new(pReport)
     when :txt
       f=TXTFormatter.new(pReport)
-    when :colored_text
-      f=ColoredTextFormatter.new(pReport)
+		when :yaml
+      f=YAMLFormatter.new(pReport)
     when :xml
       f=XMLFormatter.new(pReport)
     end
     f.init(pFilename)
     return f
-  end	
+  end
 end
---
+++ lib/formatter/yaml_formatter.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+require 'terminal-table'
+require_relative 'base_formatter'
+
+class YAMLFormatter < BaseFormatter
+
+  def initialize(pReport)
+    super(pReport)
+    @data = {}
+  end
+
+  def process
+    head = {}
+    head[:title] = "INITIAL CONFIGURATIONS"
+    @head.each { |key,value| head[key]=value.to_s }
+
+    body = {}
+    body[:title] = "HISTORY"
+    body[:lines] = []
+
+    @lines.each do |i|
+      line = ""
+      if i.class.to_s=='Hash' then
+        lValue=0.0
+        if i[:check]
+          lValue=i[:weight]
+        end
+
+        line = {}
+        line[:id]          = "%02d"%i[:id]+" ("+lValue.to_s+"/"+i[:weight].to_s+")"
+        line[:description] = i[:description].to_s
+        line[:command]     = i[:command].to_s
+        line[:duration]    = i[:duration].to_s
+        line[:alterations] = i[:alterations].to_s
+        line[:expected]    = i[:expected].to_s
+        line[:result]      = i[:result].to_s
+      else
+        line = i.to_s
+      end
+      body[:lines] << line
+    end
+
+    tail = {}
+    tail[:title] = "FINAL VALUES"
+    @tail.each { |key,value| tail[key] = value.to_s }
+
+    fame = {}
+    fame[:title] = "HALL OF FAME"
+    app=Application.instance
+    app.hall_of_fame.each { |line| fame[line[0]] = line[1] }
+
+    # Compose data ouput
+    data[:head] = head
+    data[:body] = body
+    data[:tail] = tail
+    data[:fame] = fame
+    w data.to_s # Write data into ouput file
+    deinit
+  end
+
+end
