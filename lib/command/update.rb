b44b5d9a102fb906a920d80c58565df12cc86e2b
David <dvarrui@protonmail.com>
2019-02-07 20:15:23 +0000

    F delete pull option

--- lib/command/update.rb
+++ lib/command/update.rb
@@ -1,20 +1,6 @@
 
 class Teuton < Thor
 
-  map ['--pull'] => 'pull'
-  desc 'pull', 'Pull remote files fromgit repo to current local dir'
-  long_desc  <<-LONGDESC
-  Execute "git pull".
-
-  Example:
-
-  #{$PROGRAM_NAME} pull
-
-LONGDESC
-  def pull
-    system("git pull")
-  end
-
   map ['--upgrade'] => 'upgrade'
   desc 'upgrade', 'Upgrade TEUTON software from git repo'
   long_desc  <<-LONGDESC
