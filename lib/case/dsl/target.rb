d98ee6723150c5c5a8220f0f0825a9ae5b74dd0f
David <dvarrui@protonmail.com>
2019-02-10 23:57:53 +0000

    U refactoring case#target method

--- lib/case/dsl/target.rb
+++ lib/case/dsl/target.rb
@@ -2,9 +2,23 @@
 
 module DSL
 
-  def target(text=nil)
-    return @action[:description] if text.nil?
+  def target2(text=:none)
+    return @action[:description] if text == :none
     @action[:description] = text
+  end
+
+  def target(text=:none, args={})
+    # Without params this method only return data
+    return @action[:description] if text == :none and args=={}
+
+    # With params this method modify internal data
+    @action[:description] = text.to_s
+    if args[:asset]
+      @action[:asset] = args[:asset]
+      if text == :none
+        @action[:description] = "(asset => #{args[:asset]})"
+      end
+    end
   end
   alias_method :goal, :target
 
