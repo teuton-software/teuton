e75c8a2ab09989b2f3848973a2ca4b8d5376b8b2
David <dvarrui@protonmail.com>
2019-02-17 22:00:10 +0000

    U rename upgrade method to update

--- lib/command/update.rb
+++ lib/command/update.rb
@@ -1,17 +1,17 @@
 
 class Teuton < Thor
 
-  map ['--upgrade'] => 'upgrade'
-  desc 'upgrade', 'Upgrade TEUTON software from git repo'
+  map ['--update'] => 'update'
+  desc 'update', 'Update TEUTON from git repo'
   long_desc  <<-LONGDESC
   Execute "cd PATH/TO/TEUTON/DIR && git pull".
 
   Example:
 
-  #{$PROGRAM_NAME} upgrade
+  #{$PROGRAM_NAME} update
 
 LONGDESC
-  def upgrade
+  def update
     dir = File.absolute_path(File.join(File.dirname(__FILE__), '..', '..'))
     system("cd #{dir} && git pull")
   end
