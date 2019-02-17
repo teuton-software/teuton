014aa98978ade494e7101e230ba5c6eead823c70
David <dvarrui@protonmail.com>
2019-02-17 23:20:42 +0000

    U lib/application pass rubocop validation

--- lib/application.rb
+++ lib/application.rb
@@ -1,12 +1,10 @@
-
 require 'singleton'
 
 # This Singleton contains application params
 class Application
   include Singleton
 
-  attr_reader   :name, :version, :letter, :output_basedir
-  attr_reader   :debug
+  attr_reader   :version, :letter, :output_basedir
   attr_reader   :default
   attr_accessor :verbose
   attr_accessor :options
@@ -19,17 +17,23 @@ class Application
   end
 
   def reset
-    @name = 'teuton'
     @version = '19.02.2'
     @letter = { good: '.', bad: 'F', error: '?', none: ' ' }
     @output_basedir = 'var'
-    @debug = false
-    @default = { :format => :txt }
+    @default = { name: 'teuton', format: :txt, debug: false }
     @options = {}
     @verbose = true
 
     @global = {}
     @tasks = []
     @hall_of_fame = []
+  end
+
+  def debug
+    @default[:debug]
+  end
+
+  def name
+    @default[:name]
   end
 end
