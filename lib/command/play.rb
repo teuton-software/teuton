4baaeddb45a097684a5cbbc2c450d0bd4d9ddb8c
David <dvarrui@protonmail.com>
2019-02-18 00:22:14 +0000

    D teuton#clean. Now rake clean do the same

--- lib/command/play.rb
+++ lib/command/play.rb
@@ -7,17 +7,17 @@ class Teuton < Thor
   long_desc <<-LONGDESC
 
 This function execute challenge from specified directory.
+By default, show progress on the screen.
 
-By default, it will be shown progress information on the screen.
+Available EXPORT FORMATS are: txt, colored_text, json, yaml.
 
-Available EXPORT FORMATS: txt, colored_text, json, yaml.
-
-Let's see some examples: teuton foo (Run challenge from <foo/start.rb>),
-teuton play foo (Run challenge from <foo/start.rb>),
-teuton play --export=json foo (Run challenge and export using json format),
-teuton foo/demo.rb (Run challenge from <foo/demo.rb>),
-teuton play foo/demo.rb (Run challenge from <foo/demo.rb>),
-teuton play --export=json foo/demo.rb (Export json format files)
+Let's see others options:
+(1) teuton foo (Run challenge from foo/start.rb),
+(2) teuton play foo (Run challenge from foo/start.rb),
+(3) teuton play --export=json foo (Run challenge and export using json format),
+(4) teuton foo/demo.rb (Run challenge from foo/demo.rb),
+(5) teuton play foo/demo.rb (Run challenge from foo/demo.rb),
+(6) teuton play --export=json foo/demo.rb (Export json format files)
 
 LONGDESC
   def play(path_to_rb_file)
