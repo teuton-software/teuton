bab5742532026c069d0e5114b519282f495b9de0
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-03-12 21:30:23 +0000

    U

--- bin/macosx_s-node_uninstall.sh
+++ bin/macosx_s-node_uninstall.sh
@@ -4,9 +4,13 @@
 
 [ $(whoami) != root ] && echo "[ERROR] Please, run as root" && exit 1
 
+function ssh_enabled() {
+	systemsetup -getremotelogin | grep -q "On$"
+}
+
 echo "[0/2.INFO] MacOSX S-NODE uninstallation"
 
 echo "[1/2.INFO] Disabling SSH server..."
-systemsetup -setremotelogin -f off
+ssh_enabled && systemsetup -setremotelogin off
 
 echo "[2/2.INFO] Finish!"
