a81747a15b6d9aa4e5556711c6607967fd766096
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-03-12 21:08:15 +0000

    U check run as root

--- bin/macosx_s-node_install.sh
+++ bin/macosx_s-node_install.sh
@@ -2,6 +2,8 @@
 # MacOSX S-Node Installation
 # version: 20190312
 
+[ $(whoami) != root ] && echo "[ERROR] Please, run as root" && exit 1
+
 echo "[0/2.INFO] MacOSX S-NODE installation"
 
 echo "[1/2.INFO] Enabling SSH server..."
--- bin/macosx_s-node_uninstall.sh
+++ bin/macosx_s-node_uninstall.sh
@@ -2,6 +2,8 @@
 # MacOSX S-Node Uninstallation
 # version: 20190312
 
+[ $(whoami) != root ] && echo "[ERROR] Please, run as root" && exit 1
+
 echo "[0/2.INFO] MacOSX S-NODE uninstallation"
 
 echo "[1/2.INFO] Disabling SSH server..."
