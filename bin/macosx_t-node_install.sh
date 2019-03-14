510ce33fc3e2ce726018a72ff276ecb112a9782d
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2019-03-14 22:08:54 +0000

    Update macosx_t-node_install.sh

--- bin/macosx_t-node_install.sh
+++ bin/macosx_t-node_install.sh
@@ -35,8 +35,6 @@ fi
 
 echo "Running ~/.bash_profile script" && source ~/.bash_profile
 
-source $chrubyPath/chruby.sh
-
 $(chruby | grep -q ruby) || ( echo "Installing ruby..." && ruby-install ruby )
 
 echo "Switching to new ruby version" && chruby ruby
