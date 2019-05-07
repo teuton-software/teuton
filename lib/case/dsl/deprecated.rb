740e9a756f2f16ba0a847b1de4adc452aff11889
David <dvarrui@protonmail.com>
2019-05-07 18:31:20 +0100

    U

--- lib/case/dsl/deprecated.rb
+++ lib/case/dsl/deprecated.rb
@@ -3,7 +3,14 @@
 module DSL
 
   def request(text)
-    # do nothing
+    raise 'Deprecated'
+    # do nothing by now
+  end
+
+  def target2(text=:none)
+    raise 'Deprecated'
+    return @action[:description] if text == :none
+    @action[:description] = text
   end
 
 end
