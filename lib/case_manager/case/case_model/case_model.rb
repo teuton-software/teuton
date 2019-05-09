cf2edd8c34bb1af4ff4b2afc8763d91384b5255b
David <dvarrui@protonmail.com>
2019-05-09 19:21:50 +0100

    N CaseModelTest

--- lib/case_manager/case/case_model/case_model.rb
+++ lib/case_manager/case/case_model/case_model.rb
@@ -2,17 +2,21 @@
 require_relative 'group_model'
 
 class CaseModel
+  attr_reader :groups
+
   def initialize
     @groups = []
     @current_group = nil
+  end
+
+  def group_new(name)
+    @current_group = GroupModel.new(name)
+    @groups << @current_group
+    @current_group
   end
 
   def group
     @current_group
   end
 
-  def group_new(name)
-    @current_group = GroupModel.new(name)
-    @groups << @current_group
-  end
 end
