68f72c00fa1cf0e99402ac8c781b8c0482a0d00f
David <dvarrui@protonmail.com>
2019-06-20 02:55:27 +0100

    F

--- lib/case_manager/utils.rb
+++ lib/case_manager/utils.rb
@@ -14,7 +14,7 @@ module Utils
 
   def encode_and_split(encoding, text)
 
-    text = text.encode("UTF-8", :invalid => :replace)
+    text = text.encode('UTF-8', :invalid => :replace)
     return text.split("\n")
 
     unless encoding == 'UTF-8'
