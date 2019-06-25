c31f4005695f17b65f20fb8ce2a8cac0072ba754
David <dvarrui@protonmail.com>
2019-06-25 21:26:50 +0100

    U simpler skeleton files

--- lib/files/start.rb
+++ lib/files/start.rb
@@ -1,9 +1,9 @@
 
 group "GROUP NAME" do
 
-  target "TARGET-1 DESCRIPTION"
-  goto :localhost, :exec => "COMMAND-1"
-  expect result.grep("TEXT-1").count.equal(1)
+  target "TARGET 1: DESCRIPTION"
+  goto :localhost, :exec => "COMMAND 1"
+  expect "TEXT 1"
 
 end
 
