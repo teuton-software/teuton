4841db58f835783a51f828e2570cecd817f11bbf
David <dvarrui@protonmail.com>
2019-09-01 22:17:28 +0100

    F ArrayFormatter now pass action check info to output

--- lib/application.rb
+++ lib/application.rb
@@ -17,7 +17,7 @@ class Application
   end
 
   def reset
-    @version = '2.0.6'
+    @version = '2.1.0'
     @letter = { good: '.', bad: 'F', error: '?', none: ' ' }
     @running_basedir = Dir.getwd
     @output_basedir = 'var'
--- tests/application_test.rb
+++ tests/application_test.rb
@@ -12,7 +12,7 @@ class ApplicationTest < Minitest::Test
 
   def test_version
     assert_equal 'teuton', @app.name
-    assert_equal '2.0.6', @app.version
+    assert_equal '2.1.0', @app.version
   end
 
   def test_init_params
