31913286961c8ef3575f5c5fce2aaa275199e0aa
David <dvarrui@protonmail.com>
2019-09-10 01:35:22 +0100

    F

--- lib/case_manager/export_manager.rb
+++ lib/case_manager/export_manager.rb
@@ -2,7 +2,7 @@
 
 require_relative '../application'
 
-# ExportManager#export
+# ExportManager#run
 module ExportManager
   def self.run(main_report, cases, args)
     # default :mode=>:all, :format=>:txt
@@ -11,7 +11,7 @@ module ExportManager
     # Export resume report
     main_report.export_resume format if %i[resume all].include? mode
     return unless %i[details all].include? mode
-
+    # Export case reports
     threads = []
     cases.each { |c| threads << Thread.new { c.export format } }
     threads.each(&:join)
--- lib/report/show.rb
+++ lib/report/show.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
 
 # Show methods for Report class.
-
 class Report
-
   def show
     show_initial_configurations
-    if @filename.to_s.include? "resume"
+    if @filename.to_s.include? 'resume'
       show_resume
     else
       show_targets_history
@@ -19,7 +18,7 @@ class Report
   def show_initial_configurations
     puts 'INITIAL CONFIGURATIONS'
     my_screen_table = Terminal::Table.new do |st|
-      @head.each { |key,value| st.add_row [ key.to_s, value.to_s] }
+      @head.each { |key, value| st.add_row [key.to_s, value.to_s] }
     end
     puts my_screen_table.to_s
   end
@@ -27,9 +26,9 @@ class Report
   def show_resume
     puts 'CASE RESULTS'
     my_screen_table = Terminal::Table.new do |st|
-      st.add_row [ 'CASE ID', 'GRADE', 'STATUS', 'MEMBERS' ]
+      st.add_row ['CASE ID', 'GRADE', 'STATUS', 'MEMBERS']
       @lines.each do |line|
-        st.add_row [ line[:id], line[:grade], line[:letter], line[:members] ]
+        st.add_row [line[:id], line[:grade], line[:letter], line[:members]]
       end
     end
     puts my_screen_table.to_s
@@ -38,15 +37,16 @@ class Report
   def show_targets_history
     tab = '  '
     puts 'CASE RESULTS'
-    if @lines.size==1
+    if @lines.size == 1
       puts @lines[0]
     else
       @lines.each do |i|
         if i.class.to_s == 'Hash'
           value = 0.0
           value = i[:weight] if i[:check]
-          print tab + "%03d" % i[:id] + ' (' + "%2d.2f" % value.to_f + '/'
-          puts "%2d.2f" % i[:weight].to_f + ') ' + i[:description]
+          print tab + "%03d" % i[:id].to_i
+          print ' (' + '%2d.2f' % value.to_f + '/'
+          puts '%2d.2f' % i[:weight].to_f + ') ' + i[:description].to_s
         else
           puts tab + '-  ' + i.to_s
         end
@@ -67,6 +67,7 @@ class Report
   def show_hall_of_fame
     app = Application.instance
     return if app.hall_of_fame.count < 2
+
     puts 'HALL OF FAME'
     my_screen_table = Terminal::Table.new do |st|
       app.hall_of_fame.each do |line|
