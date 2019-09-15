d9a62c4f274e3cc48f243a44a8726d37844e686f
David <dvarrui@protonmail.com>
2019-09-15 19:09:46 +0100

    U aplly rubocop to runner.rb

--- lib/case_manager/case/runner.rb
+++ lib/case_manager/case/runner.rb
@@ -8,92 +8,91 @@ class Case
   private
 
   def run_local_cmd()
-    @action[:conn_type]=:local
+    @action[:conn_type] = :local
     @result.content = my_execute( @action[:command], @action[:encoding] )
   end
 
-  def run_remote_cmd(pHostname)
-    hostname = pHostname.to_s
-    protocol = @config.get((hostname+'_protocol').to_sym) if @config.get((hostname+'_protocol').to_sym)
-    protocol = :ssh if protocol.nil? or protocol=='NODATA'
+  def run_remote_cmd(input_hostname)
+    hostname = input_hostname.to_s
+    i = (hostname + '_protocol').to_sym
+    protocol = @config.get(i) if @config.get(i)
+    protocol = :ssh if protocol.nil? || protocol == 'NODATA'
     protocol = protocol.to_sym
-
     case protocol
     when :ssh
-      run_remote_cmd_ssh(pHostname)
+      run_remote_cmd_ssh(input_hostname)
     when :telnet
-      run_remote_cmd_telnet(pHostname)
+      run_remote_cmd_telnet(input_hostname)
     else
-      raise "Unkown remote protocol <#{protocol.to_s}>"
+      raise "Unkown protocol <#{protocol.to_s}>"
     end
   end
 
-  def run_remote_cmd_ssh(pHostname)
+  def run_remote_cmd_ssh(input_hostname)
     @action[:conn_type] = :ssh
     app = Application.instance
-    hostname = pHostname.to_s
-    ip = @config.get((hostname+'_ip').to_sym)
-    username = @config.get((hostname+'_username').to_sym).to_s
-    password = @config.get((hostname+'_password').to_sym).to_s
+    hostname = input_hostname.to_s
+    ip = @config.get((hostname + '_ip').to_sym)
+    username = @config.get((hostname + '_username').to_sym).to_s
+    password = @config.get((hostname + '_password').to_sym).to_s
     text = ''
-
     begin
       if @sessions[hostname].nil?
-        @sessions[hostname] = Net::SSH.start(ip, username, :password => password)
+        @sessions[hostname] = Net::SSH.start(ip,
+                                             username,
+                                             password: password)
       end
-
-      if @sessions[hostname].class==Net::SSH::Connection::Session
-        text = @sessions[hostname].exec!( @action[:command].to_s )
+      if @sessions[hostname].class == Net::SSH::Connection::Session
+        text = @sessions[hostname].exec!(@action[:command].to_s)
       end
-
     rescue Errno::EHOSTUNREACH
-      @sessions[hostname]=:nosession
+      @sessions[hostname] = :nosession
       verbose app.letter[:error]
       log( "Host #{ip} unreachable!", :error)
     rescue Net::SSH::AuthenticationFailed
-      @sessions[hostname]=:nosession
-      verbose app.letter[:error]
-      log( "SSH::AuthenticationFailed!", :error)
-    rescue Net::SSH::HostKeyMismatch
-      @sessions[hostname]=:nosession
-      verbose app.letter[:error]
-      log('SSH::HostKeyMismatch!', :error)
-      log("* The destination server's fingerprint is not matching what is in your local known_hosts file.",:error)
-      log('* Remove the existing entry in your local known_hosts file', :error)
-      log("* Try this => ssh-keygen -f '/home/USERNAME/.ssh/known_hosts' -R #{ip}", :error)
-    rescue Exception => e
       @sessions[hostname] = :nosession
       verbose app.letter[:error]
-      log( "[#{e.class}] SSH on <#{username}@#{ip}> exec: " + @action[:command], :error)
+      log('SSH::AuthenticationFailed!', :error)
+    rescue Net::SSH::HostKeyMismatch
+      @sessions[hostname] = :nosession
+      verbose app.letter[:error]
+      log('SSH::HostKeyMismatch!', :error)
+      log("* The destination server's fingerprint is not matching " \
+          'what is in your local known_hosts file.', :error)
+      log('* Remove the existing entry in your local known_hosts file', :error)
+      log("* Try this => ssh-keygen -f '/home/USERNAME/.ssh/known_hosts' " \
+          "-R #{ip}", :error)
+    rescue StandardError => e
+      @sessions[hostname] = :nosession
+      verbose app.letter[:error]
+      log("[#{e.class}] SSH on <#{username}@#{ip}>" \
+          " exec: #{@action[:command]}", :error)
     end
-
-    output = encode_and_split(@action[:encoding],text)
-
+    output = encode_and_split(@action[:encoding], text)
     @result.content = output
     @result.content.compact!
   end
 
-  def run_remote_cmd_telnet(pHostname)
+  def run_remote_cmd_telnet(input_hostname)
     @action[:conn_type] = :telnet
     app = Application.instance
-    hostname = pHostname.to_s
-    ip = @config.get((hostname+'_ip').to_sym)
-    username = @config.get((hostname+'_username').to_sym).to_s
-    password = @config.get((hostname+'_password').to_sym).to_s
+    hostname = input_hostname.to_s
+    ip = @config.get((hostname + '_ip').to_sym)
+    username = @config.get((hostname + '_username').to_sym).to_s
+    password = @config.get((hostname + '_password').to_sym).to_s
     text = ''
-
     begin
       if @sessions[hostname].nil? || @sessions[hostname] == :ok
-        #h = Net::Telnet::new({ 'Host' => ip, 'Timeout' => 10, 'Prompt' => /[$%#>]\s?\z/n })
-        h = Net::Telnet::new({ 'Host' => ip, 'Timeout' => 10, 'Prompt' => Regexp.new(username[1,40]) })
-
+        # h = Net::Telnet::new({ 'Host' => ip, 'Timeout' => 10, 'Prompt' => /[$%#>]\s?\z/n })
+        h = Net::Telnet.new({ 'Host' => ip,
+                              'Timeout' => 10,
+                              'Prompt' => Regexp.new(username[1, 40]) })
         h.login(username, password)
         text = ''
         h.cmd(@action[:command]) { |i| text << i }
         h.close
         @sessions[hostname] = :ok
       end
-
     rescue Net::OpenTimeout
       @sessions[hostname] = :nosession
       verbose app.letter[:error]
@@ -103,16 +102,16 @@ class Case
       @sessions[hostname] = :nosession
       verbose app.letter[:error]
       log(" ExceptionType=<Net::ReadTimeout> doing <telnet #{ip}>", :error)
-    rescue Exception => e
+    rescue StandardError => e
       @sessions[hostname] = :nosession
       verbose app.letter[:error]
-      log(" ExceptionType=<#{e.class}> doing telnet on <#{username}@#{ip}> exec: " + @action[:command], :error)
-      log(" └── username=<#{username}>, password=<#{password}>, ip=<#{ip}>, HOSTID=<#{hostname}>", :warn)
+      log(" ExceptionType=<#{e.class}> doing telnet on <#{username}@#{ip}>" \
+          " exec: #{@action[:command]}", :error)
+      log(" └── username=<#{username}>, password=<#{password}>," \
+          " ip=<#{ip}>, HOSTID=<#{hostname}>", :warn)
     end
-
-    #@result.content = text.encode('UTF-8', :invalid => :replace).split("\n")
+    # @result.content = text.encode('UTF-8', :invalid => :replace).split("\n")
     output = encode_and_split(@action[:encoding], text)
-    
     @result.content = output
     @result.content.compact!
   end
