6e65dbbdda82018d3d3212890f12d7328be315e4
David <dvarrui@protonmail.com>
2019-09-20 10:37:52 +0100

    F convert target_id to Integer

--- lib/application.rb
+++ lib/application.rb
@@ -17,7 +17,7 @@ class Application
   end
 
   def reset
-    @version = '2.0.8'
+    @version = '2.0.9'
     @letter = { good: '.', bad: 'F', error: '?', none: ' ' }
     @running_basedir = Dir.getwd
     @output_basedir = 'var'
