6d2642ec5517884350ece151c619c72c23e2aa71
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 13:13:45 +0100

    Create test.ps1

---
+++ bin/test.ps1
@@ -0,0 +1,11 @@
+
+If ([System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -NotContains "S-1-5-32-544") {
+    $Host.UI.WriteErrorLine("Must be run as administrator")
+    Exit 1
+}
+
+$file = "$env:USERPROFILE\OpenSSH-Win64.zip"
+$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
+
+(New-Object System.Net.WebClient).DownloadFile($url, $file)
+
