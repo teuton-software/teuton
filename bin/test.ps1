5aa2327ab3fbe0ee56a337775a907d74921a1374
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 21:25:00 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -4,8 +4,20 @@ $wget = "http://downloads.sourceforge.net/gnuwin32/wget-1.11.4-1-setup.exe"
 $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
 $file = "$env:windir\temp\OpenSSH-Win64.zip"
 
-Write-Host "Downloading wget for Windows..."
-(New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi", "$env:USERPROFILE\7z1900-x64.msi")
-(New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-bin.zip", "$env:USERPROFILE\wget-1.11.4-1-bin.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-dep.zip", "$env:USERPROFILE\wget-1.11.4-1-dep.zip")
+$dest = "$env:windir\temp\snode"
 
+Write-Host "Creating temporary directory $dest ..."
+New-Item -ItemType Directory $dest
+
+Write-Host "Downloading and installing 7-Zip ..."
+(New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi", "$env:windir\temp\7z1900-x64.msi")
+& "$env:windir\temp\7z1900-x64.msi" /passive
+
+Write-Host "Downloading and installing wget for Windows ..."
+(New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-bin.zip", "$dest\wget-1.11.4-1-bin.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-dep.zip", "$dest\wget-1.11.4-1-dep.zip")
+& "$env:ProgramFiles\7-Zip\7z" e -y -o "$env:ProgramFiles\wget" "$dest\wget-1.11.4-1-bin.zip"
+& "$env:ProgramFiles\7-Zip\7z" e -y -o "$env:ProgramFiles\wget" "$dest\wget-1.11.4-1-dep.zip"
+
+Write-Host "Removing temporary directory $dest ..."
+Remove-Item -Recurse -Force $dest
