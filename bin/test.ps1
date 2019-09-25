eafe3ae778a3273caa82be6fdca8829adf4b7d67
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 13:22:34 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -9,5 +9,6 @@ If ([System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -NotContain
 $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
 $file = "$env:USERPROFILE\OpenSSH-Win64.zip"
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls
 Start-BitsTransfer -Source $url -Destination $file
 
