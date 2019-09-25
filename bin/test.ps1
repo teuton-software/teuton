52ddb1911e9fc07e4a163ef5af2d3e5df5295e88
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 13:21:36 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,13 @@
+Import-Module BitsTransfer
+
 
 If ([System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -NotContains "S-1-5-32-544") {
     $Host.UI.WriteErrorLine("Must be run as administrator")
     Exit 1
 }
 
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip", "$env:USERPROFILE\OpenSSH-Win64.zip")
+$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
+$file = "$env:USERPROFILE\OpenSSH-Win64.zip"
+
+Start-BitsTransfer -Source $url -Destination $file
 
