c6bd0fff85807c6bbbd846a564cb2aa6e117bf3f
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 13:15:02 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -7,5 +7,5 @@ If ([System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -NotContain
 $file = "$env:USERPROFILE\OpenSSH-Win64.zip"
 $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
 
-(New-Object System.Net.WebClient).DownloadFile($url, $file)
+(New-Object System.Net.WebClient).DownloadFile([System.Uri]$url, $file)
 
