392178c519569f11fe22c819fe54a75e08a95bdc
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 23:06:10 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -8,9 +8,6 @@ If ([System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -NotContain
     Exit 1
 }
 
-$file = "$env:windir\temp\OpenSSH-Win64.zip"
-$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
-
 function Unzip-File($zipFile, $destFolder) {
     & "$env:ProgramFiles\7-Zip\7z" "e" "-y" "-o$destFolder" "$zipFile" | Out-Null
 }
@@ -20,17 +17,25 @@ function Wget-File($url, $file) {
 }
 
 function Install-7zip() {
-    Write-Host "Downloading and installing 7-Zip ..."
-    (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi", "$env:windir\temp\7z1900-x64.msi")
-    & "$env:windir\temp\7z1900-x64.msi" /passive
-    Remove-Item "$env:windir\temp\7z1900-x64.msi"
+    If (Test-Path "$env:ProgramFiles\7-zip") {
+        Write-Host "7-Zip already installed"
+    } else {
+        Write-Host "Downloading and installing 7-Zip ..."
+        (New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi", "$env:windir\temp\7z1900-x64.msi")
+        & "$env:windir\temp\7z1900-x64.msi" /passive
+        Remove-Item "$env:windir\temp\7z1900-x64.msi"
+    }
 }
 
 function Install-Wget() {
-    Write-Host "Downloading and installing wget for Windows ..."
-    (New-Object System.Net.WebClient).DownloadFile("https://eternallybored.org/misc/wget/releases/wget-1.20.3-win64.zip", "$env:windir\temp\wget-1.20.3-win64.zip")
-    Unzip-File "$env:windir\temp\wget-1.20.3-win64.zip" "$env:ProgramFiles\wget"
-    Remove-Item "$env:windir\temp\wget-1.20.3-win64.zip"
+    If (Test-Path "$env:ProgramFiles\wget") {
+        Write-Host "wget for Windows already installed"
+    } else {
+        Write-Host "Downloading and installing wget for Windows ..."
+        (New-Object System.Net.WebClient).DownloadFile("https://eternallybored.org/misc/wget/releases/wget-1.20.3-win64.zip", "$env:windir\temp\wget-1.20.3-win64.zip")
+        Unzip-File "$env:windir\temp\wget-1.20.3-win64.zip" "$env:ProgramFiles\wget"
+        Remove-Item "$env:windir\temp\wget-1.20.3-win64.zip"
+    }
 }
 
 Write-Host "[0/5.INFO] WINDOWS S-NODE installation"
@@ -40,6 +45,9 @@ Write-Host "[1/5.INFO] Installing PACKAGES..."
 Install-7zip
 
 Install-Wget
+
+$file = "$env:windir\temp\OpenSSH-Win64.zip"
+$url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1-Beta/OpenSSH-Win64.zip"
 
 Write-Host "Downloading OpenSSH-Win64 from $url to $file..."
 Wget-File $url $file 
@@ -55,7 +63,6 @@ Set-Service sshd -StartupType Automatic
 Start-Service sshd
 
 Write-Host "[4/5.INFO] Opening TCP port 22 in Windows Firewall"
-$FirewallRuleName = "SSH TCP port 22"
-& "netsh" "advfirewall" "firewall" "add" "rule" "name=$FirewallRuleName" "dir=in" "action=allow" "protocol=TCP" "localport=22"
+& "netsh" "advfirewall" "firewall" "add" "rule" "name=SSH TCP port 22" "dir=in" "action=allow" "protocol=TCP" "localport=22" | Out-Null
 
 Write-Host "[5/5.INFO] Finish!"
