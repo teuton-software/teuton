89c54fbe10be78910d1c90c5193a74c0f9361aa1
Fran.vargas(ruiz) <4354486+fvarrui@users.noreply.github.com>
2019-09-25 21:10:11 +0100

    Update test.ps1

--- bin/test.ps1
+++ bin/test.ps1
@@ -5,7 +5,7 @@ $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.0.0.0p1
 $file = "$env:windir\temp\OpenSSH-Win64.zip"
 
 Write-Host "Downloading wget for Windows..."
+(New-Object System.Net.WebClient).DownloadFile("https://www.7-zip.org/a/7z1900-x64.msi", "$env:USERPROFILE\7z1900-x64.msi")
 (New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-bin.zip", "$env:USERPROFILE\wget-1.11.4-1-bin.zip")
-
 (New-Object System.Net.WebClient).DownloadFile("https://netix.dl.sourceforge.net/project/gnuwin32/wget/1.11.4-1/wget-1.11.4-1-dep.zip", "$env:USERPROFILE\wget-1.11.4-1-dep.zip")
 
