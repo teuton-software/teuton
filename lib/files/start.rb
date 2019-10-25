7bdd6dc925a34318856aa3db73467b79e1fc1a84
David <dvarrui@protonmail.com>
2019-10-25 20:09:46 +0100

    U refactor initial start.rb file

--- lib/files/start.rb
+++ lib/files/start.rb
@@ -1,9 +1,9 @@
 
 group "GROUP NAME" do
 
-  target "TARGET 1: DESCRIPTION"
-  goto :localhost, :exec => "COMMAND 1"
-  expect "TEXT 1"
+  target "TARGET-1 DESCRIPTION"
+  goto :localhost, :exec => "COMMAND-1"
+  expect "TEXT-1"
 
 end
 
