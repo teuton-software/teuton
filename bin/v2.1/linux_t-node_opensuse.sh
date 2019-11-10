bb80a3fff2b08c80718fb951a4c4054e64849acf
David <dvarrui@protonmail.com>
2019-11-10 19:48:33 +0000

    F zypper options

--- bin/v2.1/linux_t-node_opensuse.sh
+++ bin/v2.1/linux_t-node_opensuse.sh
@@ -1,11 +1,11 @@
 echo '[ INFO ] TEUTON installation'
 echo '[ INFO ] OpenSUSE package installation'
 zypper refresh
-zypper install --non-interactive ruby
-zypper install --non-interactive openssh
-zypper install --non-interactive make
-zypper install --non-interactive gcc
-zypper install --non-interactive ruby-devel
+zypper --non-interactive install ruby
+zypper --non-interactive install openssh
+zypper --non-interactive install make
+zypper --non-interactive install gcc
+zypper --non-interactive install ruby-devel
 
 echo '[ INFO ] Gem installation'
 gem install teuton
