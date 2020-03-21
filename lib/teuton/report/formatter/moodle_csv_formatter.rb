6b971d83eb6cd8ab8d6c18b98e182d587339a3a1
dvarrui <dvarrui@protonmail.com>
2020-03-21 18:55:12 +0000

    F moodle_id accept array or comma separated values

--- lib/teuton/report/formatter/moodle_csv_formatter.rb
+++ lib/teuton/report/formatter/moodle_csv_formatter.rb
@@ -2,13 +2,19 @@
 
 require_relative 'resume_array_formatter'
 
-# MoodleCSVFormatter class
+##
+# Format data to Moodle CSV
 class MoodleCSVFormatter < ResumeArrayFormatter
+  ##
+  # initialize instance
+  # @param report (Report)
   def initialize(report)
     super(report)
     @data = {}
   end
 
+  ##
+  # Process internal data and generates data with format
   def process
     build_data
     process_cases
@@ -21,8 +27,12 @@ class MoodleCSVFormatter < ResumeArrayFormatter
     # MoodleID, Grade, Feedback
     w "MoodleID, TeutonGrade, TeutonFeedback\n"
     @data[:cases].each do |line|
-      w "#{line[:moodle_id]},#{line[:grade]}," \
-        "#{line[:moodle_feedback]}\n" unless line[:skip]
+      moodle_id = line[:moodle_id]
+      moodle_id = line[:moodle_id].split(',') if moodle_id.class == String
+      moodle_id.each do |id|
+        w "#{id.strip},#{line[:grade]}," \
+          "#{line[:moodle_feedback]}\n" unless line[:skip]
+      end
     end
   end
 end
