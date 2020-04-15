0763de386b704b35ca24e9256b334c26cdbe393a
dvarrui <dvarrui@protonmail.com>
2020-04-15 19:06:30 +0100

    U

--- tests/teuton_test.rb
+++ tests/teuton_test.rb
@@ -6,8 +6,6 @@ require 'yaml'
 # MiniTest Rubocop
 class TeutonTest < Minitest::Test
   def setup
-    @files = []
-    @files << 'examples/learn-01-target'
   end
 
   def test_learn_01_target
@@ -26,5 +24,18 @@ class TeutonTest < Minitest::Test
     assert_equal 100, data[:cases][0][:grade]
     assert_equal 'anonymous', data[:cases][0][:members]
     assert_equal Hash.new, data[:cases][0][:conn_status]
+  end
+
+  def test_learn_02_config
+    filepath = 'examples/learn-02-config'
+    system("teuton run --no-color --export=yaml #{filepath} > /dev/null")
+    testname = File.basename(filepath)
+    resume = File.join('var', testname, 'resume.yaml')
+    data = YAML.load(File.read(resume))
+
+    assert_equal File.join(filepath, 'start.rb'), data[:config][:tt_scriptname]
+    assert_equal testname, data[:config][:tt_testname]
+
+    assert_equal 2, data[:cases].size
   end
 end
