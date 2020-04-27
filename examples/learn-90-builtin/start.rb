ed525d97a6e1ba367373186d30559501b3f5a69b
dvarrui <dvarrui@protonmail.com>
2020-04-27 10:03:49 +0100

    U

--- examples/learn-90-builtin/start.rb
+++ examples/learn-90-builtin/start.rb
@@ -4,14 +4,12 @@ group "learn-07-builtin" do
   host(:host1).service('sshd').is_running?
 
   h1 = host(:host1)
-  h1.user('root').exists?
-  h1.user('david').exists?
+  h1.package('geany').installed?
+  h1.package('basic').installed?
 
-  p1 = h1.package('geany')
-  p1.installed?
-
-  p2 = h1.package('basic')
-  p2.installed?
+  u1 = h1.user('root')
+  u1.exists?
+  u1.is_member_of? 'users'
 end
 
 play do
