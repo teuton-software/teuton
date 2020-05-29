c168f5c6321f0b56d862af11528bc372bbfad2e2
dvarrui <dvarrui@protonmail.com>
2020-05-29 20:33:44 +0100

    U

--- bin/ftp.rb
+++ bin/ftp.rb
@@ -7,10 +7,19 @@ user = 'anonymous'
 pass = 'anonymous'
 filename = ARGV[1]
 
-Net::FTP.open(host, user, pass) do |ftp|
+begin
+  ftp = Net::FTP.open(host, user, pass)
   files = ftp.list
-  puts "=> ftp> list"
+  puts "[ INFO ] list:"
   puts files
 
-  content = ftp.get(filename)
+  puts "[ INFO ] get #{filename}"
+  ftp.get(filename, "localfile-#{filename}")
+
+  ftp.close
+rescue Errno::EHOSTUNREACH
+  puts "[ ERROR ] Host unreacheble!"
+rescue Net::FTPPermError
+  puts "[ ERROR ] 550 Failed to open file!"
+  puts "          OR  Login error!"
 end
