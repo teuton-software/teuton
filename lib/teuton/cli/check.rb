a93fef3ba99b02831ea9e7d176a51b6e0adb0e74
dvarrui <dvarrui@protonmail.com>
2020-10-04 18:37:31 +0100

    U

--- lib/teuton/cli/check.rb
+++ lib/teuton/cli/check.rb
@@ -13,19 +13,19 @@ class CLI < Thor
   (1) teuton test path/to/dir/foo
   , Test content of start.rb and config.yaml files.
 
-  (2) teuton test path/to/dir/foo -c
+  (2) teuton check path/to/dir/foo -c
   , Only test CONFIG information from config.yaml.
 
-  (3) teuton test path/to/dir/foo --cname=demo
+  (3) teuton check path/to/dir/foo --cname=demo
   , Test content of start.rb and demo.yaml files.
 
-  (4) teuton test path/to/file/foo.rb
+  (4) teuton check path/to/file/foo.rb
   , Test content of foo.rb and foo.yaml files.
 
-  (5) teuton test path/to/file/foo.rb -c
+  (5) teuton check path/to/file/foo.rb -c
   , Only test CONFIG information from foo.yaml.
 
-  (6) teuton test path/to/file/foo.rb --cname=demo
+  (6) teuton check path/to/file/foo.rb --cname=demo
   , Test content of foo.rb and demo.yaml files.
 
   LONGDESC
