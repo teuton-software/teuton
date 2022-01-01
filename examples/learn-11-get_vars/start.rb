e075def7664637207ba9b58386d9a7f7e3ae087c
David Vargas <dvarrui@protonmail.com>
2022-01-01 23:59:17 +0000

    U

--- examples/learn-11-get_vars/start.rb
+++ examples/learn-11-get_vars/start.rb
@@ -1,12 +1,18 @@
 
 group "Demo new test" do
 
+	target "Exist #{get(:dirname)} directory"
+	run "file #{get(:dirname)}"
+	expect_none "No such file or directory"
+
+  dirname = get(:dirname)
+	target "Exist #{dirname} directory"
+	run "file #{dirname}"
+	expect_none "No such file or directory"
+
 	target "Exist #{_dirname_} directory"
 	run "file #{_dirname_}"
 	expect_none "No such file or directory"
-
-  puts _dirname
-	puts _dirname_
 end
 
 play do
