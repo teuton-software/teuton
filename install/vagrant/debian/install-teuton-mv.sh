24a7642cfaa3f9c8ee4eb70ae3d77d2679933364
David Vargas <dvarrui@protonmail.com>
2022-01-18 20:47:53 +0000

    U teuton-debian

---
+++ install/vagrant/debian/install-teuton-mv.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+APP_NAME=teuton
+VERSION=2.2.0
+REPO_URL=https://github.com/teuton-software/teuton-tests
+SAMPLES_FOLDER=teuton-tests
+
+apt update
+apt install -y vim tree nmap
+apt install -y neofetch
+
+echo "[INFO] Install ruby and $APP_NAME"
+apt install -y ruby irb
+gem install $APP_NAME
+
+echo "[INFO] Download $SAMPLES_FOLDER"
+apt install -y git
+git clone $REPO_URL
+chown -R vagrant:vagrant $SAMPLES_FOLDER
+
+echo "[INFO] Configure motd"
+apt install -y figlet
+figlet $APP_NAME > /etc/motd
+echo "" >> /etc/motd
+echo "$APP_NAME ($VERSION)" >> /etc/motd
+echo "$REPO_URL" >> /etc/motd
+
+echo "[INFO] Configure aliases"
+echo "# Adding more alias" >> /home/vagrant/.bashrc
+echo "alias c='clear'" >> /home/vagrant/.bashrc
+echo "alias v='vdir'" >> /home/vagrant/.bashrc
+
+lsb_release -d
