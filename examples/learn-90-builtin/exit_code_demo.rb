c19281f5b2f6bd460c6a67e58794eec36da27c08
David Vargas <dvarrui@protonmail.com>
2022-10-11 09:40:33 +0100

    [N] exit_code example to revise.

---
+++ examples/learn-90-builtin/exit_code_demo.rb
@@ -0,0 +1,19 @@
+
+use 'gnulinux/user'
+
+def exit_code(cmd, host=:localhost)
+  run cmd, on: host
+  run "echo $?", on: host
+end
+
+group "Learn about def" do
+  exit_code "id vader"
+  result.debug
+  exit_code "id root"
+  result.debug
+end
+
+play do
+  show
+  export
+end
