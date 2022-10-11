25feb3093fb594a1adf2cdd40e3313f28e0792a3
David Vargas <dvarrui@protonmail.com>
2022-10-11 09:44:56 +0100

    [U] refactoring gem installation functions.

--- tasks/install.rb
+++ tasks/install.rb
@@ -4,21 +4,13 @@ require_relative 'utils'
 namespace :install do
   desc 'Check installation'
   task :check do
-    fails = Utils.filter_uninstalled_gems(Utils.gemlist)
-    puts "[ FAIL ] Gems to install!: #{fails.join(',')}" unless fails == []
     Utils.check_tests
-  end
-
-  desc 'Install gems'
-  task :gems do
-    Utils.install_gems(Utils.packages)
   end
 
   desc 'Debian installation'
   task :debian do
     names = %w[ssh make gcc ruby-devel]
     names.each { |name| system("apt-get install -y #{name}") }
-    # Utils.install_gems Utils.packages, '--no-ri'
     Utils.create_symbolic_link
   end
 
@@ -27,7 +19,6 @@ namespace :install do
     names = %w(openssh make gcc ruby-devel)
     options = '--non-interactive'
     names.each { |n| system("zypper #{options} install #{n}") }
-    Utils.install_gems Utils.packages
     Utils.create_symbolic_link
   end
 end
