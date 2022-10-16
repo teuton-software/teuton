7360ca97130980e6cef64e8c6b51f9c815d93cd1
David Vargas <dvarrui@protonmail.com>
2022-10-16 09:11:07 +0100

    [F] new path test.

--- test/utils/configfile_reader_test.rb
+++ test/utils/configfile_reader_test.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby
 
-require "minitest/autorun"
+require "test/unit"
 require_relative "../../lib/teuton/utils/configfile_reader"
 
 # MiniTest for ConfigFileReader Class
-class ConfigFileReaderTest < Minitest::Test
+class ConfigFileReaderTest < Test::Unit::TestCase
   def test_read
-    data = ConfigFileReader.read("tests/files/example-01.yaml")
+    data = ConfigFileReader.read("test/files/example-01.yaml")
     assert_equal({}, data[:global])
     assert_equal 1, data[:cases].size
     assert_equal Hash, data[:cases][0].class
     assert_equal "student1", data[:cases][0][:tt_members]
 
-    data = ConfigFileReader.read("tests/files/example-03/config.yaml")
+    data = ConfigFileReader.read("test/files/example-03/config.yaml")
     assert_equal({}, data[:global])
     assert_equal 1, data[:cases].size
     assert_equal Hash, data[:cases][0].class
     assert_equal "student1", data[:cases][0][:tt_members]
 
-    data = ConfigFileReader.read("tests/files/example-04.json")
+    data = ConfigFileReader.read("test/files/example-04.json")
     assert_equal({}, data[:global])
     assert_equal 1, data[:cases].size
     assert_equal Hash, data[:cases][0].class
