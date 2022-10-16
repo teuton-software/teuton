183e9bb6ee7517f028acfaedf3343f098d44435f
dvarrui <dvarrui@protonmail.com>
2022-10-16 12:57:29 +0100

    [F] .

--- test/teuton_command_test.rb
+++ test/teuton_command_test.rb
@@ -6,9 +6,7 @@ class TeutonCommandTest < Test::Unit::TestCase
   def setup
     @examples = [
       "learn-01-target",
-      "learn-02-config",
-      "learn-03-remote-hosts",
-      # "learn-15-exit-codes"
+      "learn-02-config"
     ]
     filedirname = File.dirname(__FILE__)
     @dirbase = File.join(filedirname, "..", "examples")
@@ -59,7 +57,23 @@ class TeutonCommandTest < Test::Unit::TestCase
   end
 
   def test_readme_examples
-    @examples.each do |name|
+    examples = [
+      "learn-01-target",
+      "learn-02-config",
+      "learn-03-remote-hosts",
+      "learn-04-new-test",
+      "learn-05-use",
+      "learn-06-debug",
+      "learn-07-log",
+      "learn-08-readme",
+      "learn-09-preserve",
+      "learn-10-result-and-moodle_id",
+      "learn-11-get_vars",
+      "learn-12-include",
+      "learn-13-alias"
+    ]
+
+    examples.each do |name|
       dir = File.join(@dirbase, name)
 
       cmd = "teuton readme #{dir} > /dev/null"
