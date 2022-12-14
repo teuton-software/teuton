08c7c86069a0195ae11b9d4e1faa7e744cc8a606
David Vargas <dvarrui@protonmail.com>
2022-12-14 12:43:24 +0000

    [F] .

--- tasks/docker.rb
+++ tasks/docker.rb
@@ -4,8 +4,8 @@ namespace :docker do
   desc "Build docker image"
   task :build do
     puts "[INFO] Building docker image..."
-    run_cmd "docker rmi #{Teuton::DOCKERNAME}"
-    run_cmd "docker build -t #{Teuton::DOCKERNAME} install/docker/"
+    system "docker rmi #{Teuton::DOCKERNAME}"
+    system "docker build -t #{Teuton::DOCKERNAME} install/docker/"
   end
 
   desc "Push docker"
