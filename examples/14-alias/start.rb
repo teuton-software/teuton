e639b8cb3f48f81cd5af9d3074676b0d5fd47c27
David Vargas <dvarrui@protonmail.com>
2022-12-22 13:59:42 +0000

    [U] .

--- examples/14-alias/start.rb
+++ examples/14-alias/start.rb
@@ -1,12 +1,4 @@
-group "Using alias" do
-  target "Verify user #{get(:super)} with key alias."
-  run "id #{get(:super)}"
-  expect get(:super)
-
-  target "Verify user #{_username} with method alias."
-  run "id #{_username}"
-  expect _username
-end
+use 'targets'
 
 play do
   show
---
+++ examples/14-alias/targets.rb
@@ -0,0 +1,9 @@
+group "Using alias" do
+  target "Verify user #{get(:super)} with key alias."
+  run "id #{get(:super)}"
+  expect get(:super)
+
+  target "Verify user #{_username} with method alias."
+  run "id #{_username}"
+  expect _username
+end
