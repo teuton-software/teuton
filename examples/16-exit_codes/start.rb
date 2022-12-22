1289652444a5b50f456ca28c5374b935e18dd510
David Vargas <dvarrui@protonmail.com>
2022-12-22 14:06:59 +0000

    [U] .

--- examples/16-exit_codes/start.rb
+++ examples/16-exit_codes/start.rb
@@ -1,12 +1,4 @@
-group "Howto read exit code" do
-  target "No user vader"
-  run "id vader;echo $?"
-  expect_last "1"
-
-  target "Exist user root"
-  run "id root;echo $?"
-  expect_last "0"
-end
+use 'targets'
 
 play do
   show
---
+++ examples/16-exit_codes/targets.rb
@@ -0,0 +1,9 @@
+group "Howto read exit code" do
+  target "No user vader"
+  run "id vader;echo $?"
+  expect_last "1"
+
+  target "Exist user root"
+  run "id root;echo $?"
+  expect_last "0"
+end
