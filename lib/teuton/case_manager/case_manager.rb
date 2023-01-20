ceca9887d732e86185f48f077cb99d7497af8d75
dvarrui <dvarrui@protonmail.com>
2023-01-20 07:33:21 +0000

    [U] message.

--- lib/teuton/case_manager/case_manager.rb
+++ lib/teuton/case_manager/case_manager.rb
@@ -63,12 +63,12 @@ class CaseManager
   def send(args = {})
     threads = []
     puts ""
-    # puts Rainbow("-" * 50).green
-    puts Rainbow("[INFO] Sending files...#{args}").color(:green)
+    puts Rainbow("-" * 50).green
+    puts Rainbow("Sending files...#{args}").color(:green)
     @cases.each { |c| threads << Thread.new { c.send(args) } }
     threads.each(&:join)
-    puts Rainbow("[INFO] Finished").color(:green)
-    # puts Rainbow("-" * 50).green
+    puts Rainbow("Sending finished!").color(:green)
+    puts Rainbow("-" * 50).green
   end
 
   def show(options = {verbose: 1})
