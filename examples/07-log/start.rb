02f11e9bd2114626e189b0fa0bd08ba1913b9664
David Vargas <dvarrui@protonmail.com>
2023-01-24 11:03:00 +0000

    U

--- examples/07-log/start.rb
+++ examples/07-log/start.rb
@@ -1,11 +1,13 @@
 group "Learning about log messages" do
-  log "Using log messages..."
+  log "Using log messages."
 
   target "Create user david"
   run "id david"
   expect "david"
 
   log "Problem detected!", :error
+  log "This is a warning", :warn
+  log "Hi, there!", :info
 end
 
 play do
--- lib/teuton/case_manager/case/dsl/log.rb
+++ lib/teuton/case_manager/case/dsl/log.rb
@@ -7,12 +7,14 @@ module DSL
   # @param text (String)
   # @param type (Symbol) Values :info, :warn or :error
   def log(text = "", type = :info)
-    s = ""
+    s = " INFO"
     s = Rainbow("WARN!").color(:yellow) if type == :warn
     s = Rainbow("ERROR").bg(:red) if type == :error
     t = Time.now
     f = format("%<hour>02d:%<min>02d:%<sec>02d", {hour: t.hour, min: t.min, sec: t.sec})
-    @report.lines << "[#{f}] #{s}: #{text}"
+    msg = "[#{f}] #{s}: #{text}"
+    msg = "[#{f}] #{text}" if s == ""
+    @report.lines << msg
   end
   alias msg log
 end
