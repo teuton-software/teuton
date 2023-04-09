bc6d1620f1dcc39e65631ab6f722baa09e1ccdc4
dvarrui <dvarrui@protonmail.com>
2023-04-09 16:24:30 +0100

    R refactor case/dsl

---
+++ lib/teuton/case/dsl.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "dsl/expect"
+require_relative "dsl/getset"
+require_relative "dsl/goto"
+require_relative "dsl/log"
+require_relative "dsl/macro"
+require_relative "dsl/send"
+require_relative "dsl/target"
+require_relative "dsl/unique"
