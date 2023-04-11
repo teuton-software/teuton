59ef02fff6321e9e94fa0d4dc00845f0bb496ca7
David Vargas <dvarrui@protonmail.com>
2023-04-11 10:40:36 +0100

    N split application into settings and project

---
+++ lib/teuton/utils/project.rb
@@ -0,0 +1,64 @@
+# Development in progress...
+
+require_relative "name_file_finder"
+
+class Project
+  def self.init
+    @project = {}
+    @project[:running_basedir] = Dir.getwd
+    @project[:output_basedir] = "var"
+    @project[:default] = {name: "teuton", format: :txt, debug: false}
+    @project[:options] = {
+      "color" => true,
+      "lang" => "en",
+      "panel" => false
+    }
+    @project[:verbose] = true
+    @project[:global] = {} # Hash of Global configuration params
+    @project[:ialias] = {} # Hash of Internal alias
+    @project[:macros] = {} # Hash of macros
+    @project[:groups] = [] # Array of groups
+    @project[:uses] = []   # TODO: Array of files used
+    @project[:hall_of_fame] = []
+    @project[:project_path] = nil
+    @project[:script_path] = nil
+    @project[:config_path] = nil
+    @project[:test_name] = nil
+  end
+
+  def self.value
+    @project
+  end
+
+  init
+
+  def debug
+    @default[:debug]
+  end
+
+  def name
+    @default[:name]
+  end
+
+  def self.quiet?
+    return true if value[:options]["quiet"]
+    return true unless value[:verbose]
+
+    false
+  end
+
+  ##
+  # Preprocess input options:
+  # * Convert input case options String to an Array of integers
+  # * Read color input option
+  def self.add_input_params(projectpath, options)
+    value[:options].merge! options
+    NameFileFinder.find_filenames_for(projectpath)
+    Rainbow.enabled = value[:options]["color"]
+
+    unless value[:options]["case"].nil?
+      numbers = value[:options]["case"].split(",")
+      value[:options]["case"] = numbers.collect!(&:to_i)
+    end
+  end
+end
---
+++ lib/teuton/utils/settings.rb
@@ -0,0 +1,12 @@
+class Settings
+  def self.letter
+    {
+      good: ".",
+      bad: "F",
+      error: "?",
+      none: " ",
+      ok: "\u{2714}",
+      cross: "\u{2716}"
+    }
+  end
+end
