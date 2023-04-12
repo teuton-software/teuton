2920b4730d7f7ee52d3ee2bf3ae354e67c066d48
dvarrui <dvarrui@protonmail.com>
2023-04-12 21:31:36 +0100

    U

--- test/command/check_command_test.rb
+++ test/command/check_command_test.rb
@@ -2,32 +2,6 @@ require "test/unit"
 
 class CheckCommandTest < Test::Unit::TestCase
   def setup
-    @examples = [
-      "01-cmd_new",
-      "02-target",
-      "03-remote_hosts",
-      "04-config",
-      "05-use",
-      # "06-send",
-      "07-target_weight",
-      "08-unique_values",
-      "09-send",
-      "10-debug",
-      "11-export",
-      "12-preserve",
-      "13-feedback",
-      "14-moodle_id",
-      "15-readme",
-      "16-include",
-      "17-alias",
-      "18-log",
-      "19-read_vars",
-      # "20-macros",
-      "21-exit_codes",
-      "22-result",
-      "23-test-code",
-      "24-test-sql"
-    ]
     filedirname = File.dirname(__FILE__)
     @dirbase = File.join(filedirname, "..", "..", "examples")
   end
--- test/command/run_command_test.rb
+++ test/command/run_command_test.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/ruby
-
 require "test/unit"
 
 class RunCommandTest < Test::Unit::TestCase
