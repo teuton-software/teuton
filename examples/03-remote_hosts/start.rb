2ef49a003ae8156050e6bcebf06cc64e706b468e
David Vargas <dvarrui@protonmail.com>
2023-06-07 08:19:01 +0100

    U Fix example

--- examples/03-remote_hosts/start.rb
+++ examples/03-remote_hosts/start.rb
@@ -1,10 +1,10 @@
 group "Remote host" do
   target "Create user root"
-  run "id root"
+  run "id root", on: :host1
   expect ["uid=", "(root)", "gid="]
 
   target "Delete user vader"
-  run "id vader"
+  run "id vader", on: :host1
   expect ["id:", "vader", "no exist"]
 end
 
