9bbc1bedd532e8f6b80825046cfd19823ecd5589
dvarrui <dvarrui@protonmail.com>
2023-06-18 14:43:09 +0100

    U new test expect none

--- test/case/expect_any_test.rb
+++ test/case/expect_any_test.rb
@@ -1,59 +1,61 @@
 require "test/unit"
-require_relative "../../../lib/teuton/case/case"
+require_relative "../../lib/teuton/case/case"
 
-class ExpectStringTest < Test::Unit::TestCase
+class ExpectAnyTest < Test::Unit::TestCase
   def setup
     Project.init
-    Project.value[:global] = {tt_testname: "tXX_expect_string_test"}
+    Project.value[:global] = {tt_testname: "tXX_expect_any_test"}
     @case = Case.new({})
   end
 
-  def test_string
-    action = @case.action
-
+  def test_expect_any_string
     configure_result(@case, %w[a b b c])
-    @case.expect "a"
+    @case.expect_any "a"
     assert action[:check]
     assert_equal 1, action[:result]
     assert_equal "find(a) & count", action[:alterations]
     assert_equal "Greater than 0", action[:expected]
 
     configure_result(@case, %w[a b b c])
-    @case.expect "b"
+    @case.expect_any "b"
     assert action[:check]
     assert_equal 2, action[:result]
+  end
 
+  def test_expect_any_string_fail
     configure_result(@case, %w[a b b c])
-    @case.expect "d"
+    @case.expect_any "d"
     assert_equal false, action[:check]
     assert_equal 0, action[:result]
   end
 
-  def test_string_one
-    configure_result(@case, %w[a b b c])
-    @case.expect "a"
-    action = @case.action
-
+  def test_expect_any_regexp
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect_any(/aa/)
     assert action[:check]
     assert_equal 1, action[:result]
-    assert_equal "find(a) & count", action[:alterations]
+    assert_equal "find((?-mix:aa)) & count", action[:alterations]
     assert_equal "Greater than 0", action[:expected]
 
-    configure_result(@case, %w[a b b c])
-    @case.expect_one "a"
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect_any(/a/)
     assert action[:check]
-    assert_equal 1, action[:result]
+    assert_equal 3, action[:result]
 
-    configure_result(@case, %w[a b b c])
-    @case.expect_one "b"
-    assert_equal false, action[:check]
-    assert_equal 2, action[:result]
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect_any(/a|b/)
+    assert action[:check]
+    assert_equal 4, action[:result]
+  end
 
-    configure_result(@case, %w[a b b c])
-    @case.expect_one "d"
+  def test_expect_any_regexp_fail
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect_any(/d/)
     assert_equal false, action[:check]
     assert_equal 0, action[:result]
   end
+
+  def action = @case.action
 
   def configure_result(acase, content)
     acase.result.content = content
---
+++ test/case/expect_test.rb
@@ -0,0 +1,64 @@
+require "test/unit"
+require_relative "../../lib/teuton/case/case"
+
+class ExpectTest < Test::Unit::TestCase
+  def setup
+    Project.init
+    Project.value[:global] = {tt_testname: "tXX_expect_test"}
+    @case = Case.new({})
+  end
+
+  def test_expect_string
+    configure_result(@case, %w[a b b c])
+    @case.expect "a"
+    assert action[:check]
+    assert_equal 1, action[:result]
+    assert_equal "find(a) & count", action[:alterations]
+    assert_equal "Greater than 0", action[:expected]
+
+    configure_result(@case, %w[a b b c])
+    @case.expect "b"
+    assert action[:check]
+    assert_equal 2, action[:result]
+  end
+
+  def test_expect_any_string_fail
+    configure_result(@case, %w[a b b c])
+    @case.expect "d"
+    assert_equal false, action[:check]
+    assert_equal 0, action[:result]
+  end
+
+  def test_expect_regexp
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect(/aa/)
+    assert action[:check]
+    assert_equal 1, action[:result]
+    assert_equal "find((?-mix:aa)) & count", action[:alterations]
+    assert_equal "Greater than 0", action[:expected]
+
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect(/a/)
+    assert action[:check]
+    assert_equal 3, action[:result]
+
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect(/a|b/)
+    assert action[:check]
+    assert_equal 4, action[:result]
+  end
+
+  def test_expect_regexp_fail
+    configure_result(@case, %w[aa ab ac bc])
+    @case.expect(/d/)
+    assert_equal false, action[:check]
+    assert_equal 0, action[:result]
+  end
+
+  def action = @case.action
+
+  def configure_result(acase, content)
+    acase.result.content = content
+    acase.result.exitcode = 0
+  end
+end
