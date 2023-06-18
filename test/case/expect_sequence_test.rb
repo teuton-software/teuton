c716f02917cc35a8e8923258dff952b06cbbcfe6
dvarrui <dvarrui@protonmail.com>
2023-06-18 16:58:53 +0100

    U

--- test/case/expect_sequence_test.rb
+++ test/case/expect_sequence_test.rb
@@ -8,7 +8,7 @@ class ExpectSequenceTest < Test::Unit::TestCase
     @case = Case.new({})
   end
 
-  def test_expect_sequence_find
+  def test_expect_sequence_find_strings
     configure_result(@case, %w[a1 b2 c3 d4 e5])
     @case.expect_sequence do
       find "a1"
@@ -30,6 +30,24 @@ class ExpectSequenceTest < Test::Unit::TestCase
     assert_equal "find(a1) then find(c3) then find(e5)", action[:result]
     assert_equal "", action[:alterations]
     assert_equal "find(a1) then find(c3) then find(e5)", action[:expected]
+  end
+
+  def test_expect_sequence_find_regexps
+    configure_result(@case, %w[a1 b2 c3 d4 e5])
+    @case.expect_sequence do
+      find(/a/)
+      find(/b/)
+      find(/c/)
+    end
+    assert action[:check]
+
+    configure_result(@case, %w[a1 b2 c3 d4 e5])
+    @case.expect_sequence do
+      find(/a/)
+      find(/c/)
+      find(/e/)
+    end
+    assert action[:check]
   end
 
   def test_expect_sequence_followed_by
