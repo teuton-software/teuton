e58f6166d85ad3379856ec4b3ac30c1f65cd753e
dvarrui <dvarrui@protonmail.com>
2023-07-21 22:04:41 +0100

    [U]

--- examples/23-test-code/start.rb
+++ examples/23-test-code/start.rb
@@ -1,11 +1,11 @@
 group "Test code example" do
   filepath = "./#{get(:folder)}/#{get(:filename)}"
 
-  target "Sum"
+  target "Script calculates the sum of 2 numbers"
   run "#{filepath} 3 4"
   expect ["Sum", "7"]
 
-  target "Mul"
+  target "Script calculates the multiplication of 2 numbers"
   run "#{filepath} 3 4"
   expect(/Mul\s+=\s+12/)
 end
