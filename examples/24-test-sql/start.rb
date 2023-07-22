5ca44ce28b93501a72c51ef0d94774d49ecebb69
dvarrui <dvarrui@protonmail.com>
2023-07-22 09:56:47 +0100

    [F]

--- examples/24-test-sql/start.rb
+++ examples/24-test-sql/start.rb
@@ -3,10 +3,15 @@ group "Test SQL and database" do
   query = "#{get(:folder)}/#{get(:query)}"
 
   target "Database schema"
+  readme "Create database '#{database}'"
+  readme "Create schema 'characters'"
+  readme "With '[name varchar, rol varchar]'"
   run "sqlite3 #{database} '.schema characters'"
   expect ["name varchar", "rol varchar"]
 
   target "Query Jedi"
+  readme "Insert '[Obiwan, Jedi]' into 'characters'"
+  readme "Create query file '#{query}' that show all rows from 'characters'"
   run "sqlite3 #{database} '.read #{query}'"
   expect ["Obiwan", "Jedi"]
 end
