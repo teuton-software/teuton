95a679fc3601a19f60dbc7ae41eb422b7e25b280
dvarrui <dvarrui@protonmail.com>
2023-07-22 09:48:08 +0100

    [U] better example readme message

--- examples/23-test-code/start.rb
+++ examples/23-test-code/start.rb
@@ -2,10 +2,14 @@ group "Test code example" do
   filepath = "./#{get(:folder)}/#{get(:filename)}"
 
   target "Script calculates the sum of 2 numbers"
+  readme "When script args are '4 5'"
+  readme "Then show output with: 'Sum = 9'"
   run "#{filepath} 3 4"
   expect ["Sum", "7"]
 
   target "Script calculates the multiplication of 2 numbers"
+  readme "When script args are '4 5'"
+  readme "Then show output with: 'Mul = 20'"
   run "#{filepath} 3 4"
   expect(/Mul\s+=\s+12/)
 end
