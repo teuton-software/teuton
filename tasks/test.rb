43170a63c6a8b281fffbd14a45e767956c732c28
David Vargas <dvarrui@protonmail.com>
2025-11-20 22:31:50 +0000

    add task description

--- tasks/test.rb
+++ tasks/test.rb
@@ -1,11 +1,12 @@
 namespace :test do
-  desc "Run tests (excluding slow tests)"
   Rake::TestTask.new(:fast) do |t|
+    t.description = "Run fast tests (excluding slow tests)"
     t.libs << "test"
     t.test_files = FileList["test/**/*_test.rb"].exclude(/slow_|test_slow/)
   end
 
   Rake::TestTask.new(:all) do |t|
+    t.description = "Run all tests (including slow tests)"
     t.libs << "test"
     t.test_files = FileList["test/**/*_test.rb"]
   end
