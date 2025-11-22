5e49bd48054276b8f80f2471ff46d9cae9ff7836
David Vargas <dvarrui@protonmail.com>
2025-11-22 20:35:35 +0000

    new feature teuton config server creator

---
+++ lib/teuton/config/server.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'sinatra'
+
+get '/' do
+  @names = ["tt_members", "host1_ip", "host1_username", "host1_password"]
+  erb :form
+end
+
+post '/submit' do
+  # Los datos del formulario se encuentran en el objeto 'params'
+  erb :feedback
+end
