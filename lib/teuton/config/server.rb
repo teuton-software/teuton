9767f4c5112e89743ea3d0cec9073c5ab98ffa6c
David Vargas <dvarrui@protonmail.com>
2025-11-24 00:20:55 +0000

    moving code into class

--- lib/teuton/config/server.rb
+++ lib/teuton/config/server.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby
 
-require "sinatra"
+require "sinatra/base"
 require_relative "../utils/config_file_reader"
 require_relative "../utils/name_file_finder"
 
-get "/" do
-  projectpath = "examples/03-remote_hosts"
-  finder = NameFileFinder.new
-  finder.find_filenames_for(projectpath)
-  config_path = finder.config_path
+class ConfigServer < Sinatra::Base
+  get "/" do
+    projectpath = "examples/03-remote_hosts"
+    finder = NameFileFinder.new
+    finder.find_filenames_for(projectpath)
+    config_path = finder.config_path
 
-  data = ConfigFileReader.read(config_path)
-  names = data[:cases].first.keys
-  erb :form, locals: {names: names}
+    data = ConfigFileReader.read(config_path)
+    names = data[:cases].first.keys
+    erb :form, locals: {names: names}
+  end
+
+  post "/submit" do
+    # Los datos del formulario se encuentran en el objeto 'params'
+    erb :feedback
+  end
+
+  at_exit do
+    puts "Closing ConfigServer"
+  end
 end
 
-post "/submit" do
-  # Los datos del formulario se encuentran en el objeto 'params'
-  erb :feedback
-end
+ConfigServer.run!
