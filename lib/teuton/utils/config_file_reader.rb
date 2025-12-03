062886706189a9092d5d3677cc7df7ab4e79822d
David Vargas <dvarrui@protonmail.com>
2025-12-03 19:12:20 +0000

    refactor: config file reader

--- lib/teuton/utils/config_file_reader.rb
+++ lib/teuton/utils/config_file_reader.rb
@@ -14,12 +14,47 @@ module ConfigFileReader
     raise "[ERROR] ConfigFileReader.read: <#{filepath}>. Unkown extension!"
   end
 
+  def self.convert_string_keys_to_symbol(input)
+    return input if input.class != Hash
+
+    output = {}
+    input.each_pair do |key, value|
+      key2 = key
+      key2 = key.to_sym if key.class
+      value2 = value
+      if value.instance_of? Hash
+        value2 = convert_string_keys_to_symbol(value)
+      elsif value.instance_of? Array
+        value2 = []
+        value.each { |i| value2 << convert_string_keys_to_symbol(i) }
+      end
+      output[key2] = value2
+    end
+    output
+  end
+
   def self.minimum_configuration_with_one_case
     data = {}
     data[:global] = {}
     data[:alias] = {}
     data[:cases] = [{tt_members: "anonymous"}]
     data
+  end
+
+  def self.is_yaml_file?(filepath)
+    extensions = [".yaml", ".YAML", ".yml", ".YML"]
+    ext = File.extname(filepath)
+    return true if File.file?(filepath) && extensions.include?(ext)
+
+    false
+  end
+
+  def self.is_json_file?(filepath)
+    extensions = [".json", ".JSON"]
+    ext = File.extname(filepath)
+    return true if File.file?(filepath) && extensions.include?(ext)
+
+    false
   end
 
   def self.read_yaml(filepath)
@@ -35,7 +70,7 @@ module ConfigFileReader
     data[:alias] = data[:alias] || {}
     data[:cases] = data[:cases] || []
     read_included_files!(filepath, data)
-    convert_string_keys_to_symbol(data)
+    data
   end
 
   def self.read_json(filepath)
@@ -45,7 +80,7 @@ module ConfigFileReader
     data[:alias] = data[:alias] || {}
     data[:cases] = data[:cases] || []
     read_included_files!(filepath, data)
-    convert_string_keys_to_symbol(data)
+    data
   end
 
   def self.read_included_files!(filepath, data)
@@ -57,28 +92,23 @@ module ConfigFileReader
     else
       File.join(File.dirname(filepath), data[:global][:tt_include])
     end
-    exts = {
-      yaml: [".yaml", ".YAML", ".yml", ".YML"],
-      json: [".json", ".JSON"]
-    }
     filepaths = Dir.glob(File.join(basedir, "**/*"))
     filepaths.each { |filepath|
-      ext = File.extname(filepath)
       if File.directory?(filepath)
         next
-      elsif exts[:yaml].include? ext
+      elsif is_yaml_file? filepath
         case_params = read_included_yaml_file(filepath)
         case_params[:tt_source_file] = relative_path(filepath)
         data[:cases] << case_params
-      elsif exts[:json].include? ext
+      elsif is_json_file? filepath
         case_params = read_included_json_file(filepath)
         case_params[:tt_source_file] = relative_path(filepath)
         data[:cases] << case_params
       elsif File.file? filepath
-        warn "[WARN] Ignore config file <#{filename}>. No yaml or json extension!"
+        warn "[WARN] Ignore config file <#{filepath}>. No yaml or json extension!"
         next
       end
-  }
+    }
   end
 
   def self.read_included_yaml_file(filepath)
@@ -105,28 +135,9 @@ module ConfigFileReader
 
   def self.relative_path(filepath)
     if filepath.start_with?(Dir.pwd)
-      filepath[Dir.pwd.length, filepath.length]
+      filepath[Dir.pwd.length + 1, filepath.length]
     else
       filepath
     end
-  end
-
-  def self.convert_string_keys_to_symbol(input)
-    return input if input.class != Hash
-
-    output = {}
-    input.each_pair do |key, value|
-      key2 = key
-      key2 = key.to_sym if key.class
-      value2 = value
-      if value.instance_of? Hash
-        value2 = convert_string_keys_to_symbol(value)
-      elsif value.instance_of? Array
-        value2 = []
-        value.each { |i| value2 << convert_string_keys_to_symbol(i) }
-      end
-      output[key2] = value2
-    end
-    output
   end
 end
