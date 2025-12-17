a9b0bf3bb2a75293645443ad07121e66bab7adfa
David Vargas <dvarrui@protonmail.com>
2025-12-17 21:29:33 +0000

    fix: getset error with symbol values

--- lib/teuton/readme/dsl/getset.rb
+++ lib/teuton/readme/dsl/getset.rb
@@ -14,13 +14,14 @@ module ReadmeDSL
   def gett(value)
     a = get(value)
     if @cases_params.include? value
-      "[" + value + "](#required-params)"
+      "[#{value}](#required-params)"
     elsif @setted_params[value]
-      "[" + value + "](#created-params)"
+      "[#{value}](#created-params)"
     elsif @global_params.include? value
-      "[" + a + "](#global-params)"
+      "[#{a}](#global-params)"
+    else
+      a
     end
-    a
   end
 
   def set(key, value)
